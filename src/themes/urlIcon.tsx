9f4c577a2d8df4967b1324129193b7b358b4e93c
Wassim ayedi <wassim.ayadi@med.tn>
2022-06-07 15:42:18 +0100

    actes

--- src/themes/urlIcon.tsx
+++ src/themes/urlIcon.tsx
@@ -3,11 +3,12 @@ import { ReactSVG } from "react-svg";
 
 interface iconUrl{
     path?: string
+    onChange?:any
     className?: string
     variant?: string
 }
 
-export default function IconUrl({ path, className, ...props }: iconUrl) {
+export default function IconUrl({ path, onChange, className, ...props }: iconUrl) {
     const prefix = "/static/icons/";
-    return <ReactSVG {...props} className={`react-svg ${className ? className : ''}`} src={`${prefix}${path}.svg`} />;
+    return <ReactSVG  {...props} onClick={onChange} className={`react-svg ${className ? className : ''}`} src={`${prefix}${path}.svg`} />;
 }
