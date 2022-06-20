e1686b497eda340f3a6ce037e7a9cd1c9a8f4069
mohamed.zghal <mohamed.zghal@med.tn>
2022-06-20 10:04:48 +0100

    add globe button logic

--- src/features/sideBarMenu/components/sideBarMenu.tsx
+++ src/features/sideBarMenu/components/sideBarMenu.tsx
@@ -45,9 +45,16 @@ function SideBarMenu({ children }: LayoutProps) {
         container.current = document.body as HTMLDivElement;
     })
 
+    const handleRouting = (path: string) => {
+        // Always do navigations after the first render
+        router.push(path);
+    }
+
 
     const { t, ready } = useTranslation("menu");
     if (!ready) return (<>loading translations...</>);
+
+
 
     const handleClick = (path:string) => {
         if (path === "/settings") {
@@ -73,7 +80,7 @@ function SideBarMenu({ children }: LayoutProps) {
             <List>
                 {sidebarItems.map((item) => (
                     <Hidden key={item.name} smUp={item.name === "wallet"}>
-                        <Link href={item.href} passHref>
+                        <a onClick={() => handleRouting(item.href)}>
                             <ListItem
                                 disableRipple
                                 button
@@ -83,7 +90,7 @@ function SideBarMenu({ children }: LayoutProps) {
                                 </ListItemIcon>
                                 <ListItemTextStyled primary={t("main-menu." + item.name)} />
                             </ListItem>
-                        </Link>
+                        </a>
                     </Hidden>
                 ))}
             </List>
