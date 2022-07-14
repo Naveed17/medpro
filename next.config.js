a92a36ef0e54031768b458d4b21bcd44e7232058
mohamed.zghal <mohamed.zghal@med.tn>
2022-07-14 11:56:28 +0100

    upgrade nextjs to stable version update middleware and moved to one single file

--- next.config.js
+++ next.config.js
@@ -1,16 +1,27 @@
-const { i18n } = require("./next-i18next.config");
+const {i18n} = require("./next-i18next.config");
 const {withTM} = require("./next-fullcalendar.config");
+const path = require("path");
 
 /** @type {{}} */
 const nextConfig = withTM({
-  i18n,
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack', 'url-loader']
-    })
-    return config
-  }
+    i18n,
+    webpack: (config,  { nextRuntime }) => {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack', 'url-loader']
+        })
+        // Undocumented property of next 12.
+        if (nextRuntime !== "nodejs") return config;
+        return {
+            ...config,
+            entry() {
+                return config.entry().then((entry) => ({
+                    ...entry,
+                    cli: path.resolve(process.cwd(), "lib/cli.ts"),
+                }));
+            },
+        };
+    }
 })
 
 module.exports = nextConfig
---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+// eslint-disable-next-line @next/next/no-server-import-in-page
+import {NextResponse} from "next/server";
+// eslint-disable-next-line @next/next/no-server-import-in-page
+import type {NextRequest} from "next/server"
+import type {JWT} from "next-auth/jwt"
+import {withAuth} from "next-auth/middleware"
+
+export default withAuth(
+    // @ts-ignore
+    async function middleware(req: NextRequest & { nextauth: { token: JWT } }) {
+        if (req.nextUrl.pathname.startsWith('/dashboard')) {
+            const {data: user} = req.nextauth.token as any;
+            const medical_professional: MedicalProfessionalModel = user.medical_professional;
+            if (medical_professional !== undefined && medical_professional.registrationStep < 3) {
+                return NextResponse.rewrite(
+                    new URL(`/edit-profile?from=${req.nextUrl.pathname}&step=${medical_professional.registrationStep}`, req.url)
+                )
+            }
+        }
+    },
+    {
+        callbacks: {
+            authorized({req, token}) {
+                if (token) return true // If there is a token, the user is authenticated
+            }
+        },
+    }
+)
+
+export const config = {matcher: ["/dashboard/:path*", "/edit-profile"]}
