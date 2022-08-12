49e8973014871140414b576ac1808590eed2169c
mohamed.zghal <mohamed.zghal@med.tn>
2022-08-12 09:25:59 +0100

    update openingHoursModel interface

--- src/interfaces/openingHoursModel.d.ts
+++ src/interfaces/openingHoursModel.d.ts
@@ -1,6 +1,5 @@
-interface openingHoursModel {
-    isMain: boolean;
-    isVisible: boolean;
-    openingHours: Array<{ [key: string]: Array<{ end_time: string, start_time: string }> }>;
-    uuid: string;
+interface OpeningHoursModel {
+  isVisible: boolean;
+  isMain: boolean;
+  openingHours: any;
 }
