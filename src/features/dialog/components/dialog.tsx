ddc5cc314e98071f38fb1a6a983e3235499ca2f7
“kamranansariofficial” <kamranansari5580@gmail.com>
2022-08-23 10:36:26 +0500

    fixed error

--- src/features/dialog/components/dialog.tsx
+++ src/features/dialog/components/dialog.tsx
@@ -5,36 +5,41 @@ import { DialogActions, DialogContent, DialogContentText } from "@mui/material";
 import { Theme } from "@mui/material/styles";
 
 function SettingsDialogs({ ...props }) {
-    const selectted = DialogData.find((item) => item.action === props.action);
+  const selectted = DialogData.find((item) => item.action === props.action);
 
-    const data = props.data;
-    const { action } = props;
-    const Component: any = selectted ? selectted.component : action;
+  const data = props.data;
+  const { action } = props;
+  const Component: any = selectted ? selectted.component : action;
 
-    return (
-        <>
-            <Dialog
-                open={props.open}
-                onClose={props.dialogClose}
-                scroll="paper"
-                {...(props.max ? { maxWidth: 'lg' } : { maxWidth: 'sm' })}
-                dir={props.direction}
-                aria-labelledby="scroll-dialog-title"
-                aria-describedby="scroll-dialog-description">
-                <DialogTitle sx={{
-                    backgroundColor: props.color ? props.color : (theme: Theme) => theme.palette.primary.main
-                }}
-                    id="scroll-dialog-title">{props.title}</DialogTitle>
-                <DialogContent dividers={true}>
-                    <DialogContentText
-                        id="scroll-dialog-description"
-                        tabIndex={-1} />
-                    <Component data={data} />
-                </DialogContent>
-                <DialogActions>{props.actionDialog}</DialogActions>
-            </Dialog>
-        </>
-    );
+  return (
+    <>
+      <Dialog
+        open={props.open}
+        onClose={props.dialogClose}
+        scroll="paper"
+        {...(props.max ? { maxWidth: "lg" } : { maxWidth: "sm" })}
+        dir={props.direction}
+        aria-labelledby="scroll-dialog-title"
+        aria-describedby="scroll-dialog-description"
+      >
+        <DialogTitle
+          sx={{
+            backgroundColor: props.color
+              ? props.color
+              : (theme: Theme) => theme.palette.primary.main,
+          }}
+          id="scroll-dialog-title"
+        >
+          {props.title}
+        </DialogTitle>
+        <DialogContent dividers={true}>
+          <DialogContentText id="scroll-dialog-description" tabIndex={-1} />
+          <Component data={data} {...props} />
+        </DialogContent>
+        <DialogActions>{props.actionDialog}</DialogActions>
+      </Dialog>
+    </>
+  );
 }
 
 export default SettingsDialogs;
