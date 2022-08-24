03ad62f76310220ce4ac53985f4b2f7f794c5991
Wassim ayedi <wassim.ayadi@med.tn>
2022-08-24 10:57:34 +0100

    useDateConverture warrning

--- src/app/hooks/useDateConverture.ts
+++ src/app/hooks/useDateConverture.ts
@@ -1,4 +1,4 @@
-import {useRef, useEffect, useState} from "react";
+import {useRef} from "react";
 import moment from "moment";
 
 // ----------------------------------------------------------------------
@@ -17,31 +17,28 @@ function useDateConverture(min: number, max: number) {
     const convertDate = useRef<any[]>([]);
     let minutes: number[] = [];
     let diff = 1;
-    useEffect(
-        () => {
-            convertDate.current = [];
+    convertDate.current = [];
 
-            do {
-                if (min >= 1440) diff = 1440;
-                else if (min >= 60) diff = 60;
-                else if (min >= 30) diff = 15;
-                else if (min >= 5) diff = 5;
+    do {
+        if (min >= 1440) diff = 1440;
+        else if (min >= 60) diff = 60;
+        else if (min >= 30) diff = 15;
+        else if (min >= 5) diff = 5;
 
-                minutes = [...minutes, min]
-                min += diff;
+        minutes = [...minutes, min]
+        min += diff;
 
-            } while (min <= max)
+    } while (min <= max)
 
-            minutes.map((minute: number) => {
-                const dates = Object.values(((moment.duration(minute, "minutes") as any)._data));
-                const index = dates.reverse().findIndex((date) => date !== 0);
-                convertDate.current = [...convertDate.current, {
-                    value:minute,
-                    date: dates[index],
-                    unity:(dates[index] === 1? unities[index].singular:unities[index].plural)
-                }];
-            })
-        });
+    minutes.map((minute: number) => {
+        const dates = Object.values(((moment.duration(minute, "minutes") as any)._data));
+        const index = dates.reverse().findIndex((date) => date !== 0);
+        convertDate.current = [...convertDate.current, {
+            value:minute,
+            date: dates[index],
+            unity:(dates[index] === 1? unities[index].singular:unities[index].plural)
+        }];
+    })
     return convertDate.current;
 }
 
