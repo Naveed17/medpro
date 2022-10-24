a1b3510431efa099a0b45b66c2d89f05276be1c7
Wassim ayedi <wassim.ayadi@med.tn>
2022-10-24 15:42:35 +0100

    gallery concept

---
+++ src/interfaces/MedicalDocuments.d.ts
@@ -0,0 +1,10 @@
+interface MedicalDocuments {
+  title: string,
+  createdAt:string,
+  documentType:string
+  medical_imaging:any[]
+  prescription:any[]
+  requested_Analyses:any[]
+  uri: string
+  uuid:string
+}
