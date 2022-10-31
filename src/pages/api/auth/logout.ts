8015ab01938c4bb8b640d2352d96ecc092b44b07
mohamed.zghal <mohamed.zghal@med.tn>
2022-10-31 11:48:39 +0100

    fix: add logout url variable

--- src/pages/api/auth/logout.ts
+++ src/pages/api/auth/logout.ts
@@ -1,9 +1,9 @@
 import {NextApiRequest, NextApiResponse} from "next";
 
-const NEXTAUTH_URL = process.env.NEXTAUTH_URL
+const NEXTAUTH_LOGOUT_URL = process.env.NEXTAUTH_LOGOUT_URL
 
 export default async function logout(req: NextApiRequest, res: NextApiResponse) {
-    const path = `${process.env.KEYCLOAK_ISSUER}/protocol/openid-connect/logout?redirect_uri=https://www.med.tn/praticien/deconnecter`;
+    const path = `${process.env.KEYCLOAK_ISSUER}/protocol/openid-connect/logout?redirect_uri=${NEXTAUTH_LOGOUT_URL}`;
 
     res.status(200).json({ path });
 };
