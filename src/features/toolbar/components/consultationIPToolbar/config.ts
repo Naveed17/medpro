def600205c4f4fe728a613aab56a6e4e4ae61238
Wassim ayedi <wassim.ayadi@med.tn>
2022-11-09 11:49:00 +0100

    fix: hide write free doc

--- src/features/toolbar/components/consultationIPToolbar/config.ts
+++ src/features/toolbar/components/consultationIPToolbar/config.ts
@@ -15,10 +15,10 @@ export const documentButtonList = [
         icon: "ic-analyse",
         label: "balance_sheet_request",
     },
-    {
+    /*{
         icon: "ic-color-edit-file",
         label: "write_report",
-    },
+    },*/
     {
         icon: "ic-dowlaodfile",
         label: "upload_document",
