f8c12167627a2544e9e107d9ed4e5827f71f0796
mohamed.zghal <mohamed.zghal@med.tn>
2022-12-13 11:36:40 +0100

    feat: update act fees behavior

--- src/pages/dashboard/settings/actfees/index.tsx
+++ src/pages/dashboard/settings/actfees/index.tsx
@@ -1,17 +1,27 @@
 import {GetStaticProps} from "next";
 import {serverSideTranslations} from "next-i18next/serverSideTranslations";
-import React, {ReactElement, useEffect, useState} from "react";
+import React, {ReactElement, useCallback, useEffect, useState} from "react";
 import {DashLayout} from "@features/base";
 import {useSession} from "next-auth/react";
 import {Session} from "next-auth";
-import {Box, Button, IconButton, InputAdornment, Stack, TextField, Typography, useTheme} from "@mui/material";
+import {
+    Autocomplete,
+    Box,
+    Button, createFilterOptions,
+    IconButton,
+    InputAdornment,
+    Stack,
+    TextField,
+    Typography,
+    useTheme
+} from "@mui/material";
 import {useTranslation} from "next-i18next";
 import {useRequest, useRequestMutation} from "@app/axios";
 import {useRouter} from "next/router";
 import {RootStyled} from "@features/toolbar";
 import {SubHeader} from "@features/subHeader";
 import {Otable} from '@features/table';
-import {TriggerWithoutValidation} from "@app/swr/swrProvider";
+import {SWRNoValidateConfig, TriggerWithoutValidation} from "@app/swr/swrProvider";
 import SaveRoundedIcon from "@mui/icons-material/SaveRounded";
 import {useSnackbar} from "notistack";
 import {LoadingScreen} from "@features/loadingScreen";
@@ -24,6 +34,8 @@ interface HeadCell {
     sortable: boolean;
     align: "left" | "right" | "center";
 }
+
+const filter = createFilterOptions<any>();
 
 const headCells: readonly HeadCell[] = [
     {
@@ -49,41 +61,47 @@ const headCells: readonly HeadCell[] = [
         label: "actions",
         sortable: true,
         align: "right",
-    },
-
+    }
 ];
 
 function ActFees() {
     const {data: session} = useSession();
     const theme = useTheme();
     const devise = process.env.NEXT_PUBLIC_DEVISE;
+    const router = useRouter();
+
     const [mainActes, setMainActes] = useState<any>([]);
     const [loading, setLoading] = useState<boolean>(false)
     const [create, setCreate] = useState(false)
-    const router = useRouter();
-    //const [selected, setselected] = useState<any>({});
     const [consultationFees, setConsultationFees] = useState(0);
-    const [newFees, setNewFees] = useState({name: '', fees: ''});
+    const [newFees, setNewFees] = useState<{ act: ActModel | string | null, fees: string }>({act: null, fees: ''});
     const {enqueueSnackbar} = useSnackbar();
-
 
     const {data: user} = session as Session;
     const medical_entity = (user as UserDataResponse).medical_entity as MedicalEntityModel;
     const medical_professional = (user as UserDataResponse).medical_professional as MedicalProfessionalModel;
 
     const {trigger} = useRequestMutation(null, "/settings/acts");
+    const {trigger: triggerAddAct} = useRequestMutation(null, "/settings/acts/add");
+
+    const {data: httpActSpeciality} = useRequest(medical_professional ? {
+        method: "GET",
+        url: `/api/public/acts/${router.locale}`,
+        params: {['specialities[0]']: medical_professional.specialities[0].speciality.uuid},
+        headers: {Authorization: `Bearer ${session?.accessToken}`}
+    } : null);
 
     const {data: httpProfessionalsActs, mutate} = useRequest({
         method: "GET",
         url: `/api/medical-entity/${medical_entity.uuid}/professionals/${medical_professional.uuid}/acts/${router.locale}`,
         headers: {Authorization: `Bearer ${session?.accessToken}`}
-    });
+    }, SWRNoValidateConfig);
 
     const {data: httpMPResponse} = useRequest({
         method: "GET",
         url: `/api/medical-entity/${medical_entity?.uuid}/professionals/${router.locale}`,
         headers: {Authorization: `Bearer ${session?.accessToken}`},
-    });
+    }, SWRNoValidateConfig);
 
     useEffect(() => {
         if (httpMPResponse) {
@@ -106,7 +124,8 @@ function ActFees() {
     }
 
     const handleRemove = () => {
-        setCreate(false)
+        setCreate(false);
+        setNewFees({act: null, fees: ''});
     }
 
     const editFees = () => {
@@ -119,8 +138,7 @@ function ActFees() {
             headers: {
                 Authorization: `Bearer ${session?.accessToken}`
             }
-        }, TriggerWithoutValidation).then(() => enqueueSnackbar(t("feesupdated"), {variant: 'success'})
-        );
+        }, TriggerWithoutValidation).then(() => enqueueSnackbar(t("feesupdated"), {variant: 'success'}));
     }
 
     const removeFees = (uuid: string) => {
@@ -138,12 +156,13 @@ function ActFees() {
     }
 
     const saveFees = () => {
-        if (newFees.name !== '' && newFees.fees !== '') {
+        if (newFees.fees !== '' && typeof newFees.act === 'string') {
             const form = new FormData();
             form.append('name', JSON.stringify({
-                "fr": newFees.name,
+                "fr": newFees.act
             }));
-            form.append('price', `${newFees.fees}`)
+            form.append('price', `${newFees.fees}`);
+
             trigger({
                 method: "POST",
                 url: `/api/medical-entity/${medical_entity.uuid}/professionals/${medical_professional.uuid}/new-acts/${router.locale}`,
@@ -154,35 +173,55 @@ function ActFees() {
             }, TriggerWithoutValidation).then(() => {
                 mutate().then(() => {
                     setCreate(false)
-                    setNewFees({name: '', fees: ''})
+                    setNewFees({act: null, fees: ''})
                     enqueueSnackbar(t("addedfees"), {variant: 'success'})
                 })
             })
         }
     }
 
+    const setActFees = useCallback((isTopAct: boolean, actFees: { act: ActModel | string | null, fees: string }) => {
+        const form = new FormData();
+        form.append('topAct', isTopAct.toString());
+        form.append('act', (actFees.act as ActModel)?.uuid);
+        triggerAddAct({
+            method: "POST",
+            url: "/api/medical-entity/" + medical_entity.uuid + "/professionals/" + medical_professional.uuid + '/acts/' + router.locale,
+            data: form,
+            headers: {
+                ContentType: 'application/x-www-form-urlencoded',
+                Authorization: `Bearer ${session?.accessToken}`
+            }
+        }, TriggerWithoutValidation).then(() => {
+            handleEdit(actFees, actFees.fees);
+        })
+
+    }, [medical_entity.uuid, medical_professional.uuid, mutate, router.locale, session?.accessToken, triggerAddAct]); // eslint-disable-line react-hooks/exhaustive-deps
+
     const handleEdit = (v: any, fees: string) => {
-        console.log(v)
         const form = new FormData();
         form.append("attribute", "price");
         form.append("value", fees);
 
         trigger({
             method: "PATCH",
-            url: `/api/medical-entity/${medical_entity.uuid}/professionals/${medical_professional.uuid}/acts/${v.act.uuid}/${router.locale}`,
+            url: `/api/medical-entity/${medical_entity.uuid}/professionals/${medical_professional.uuid}/acts/${v.act?.uuid}/${router.locale}`,
             data: form,
             headers: {
                 Authorization: `Bearer ${session?.accessToken}`
             },
         }, TriggerWithoutValidation).then(() => {
             mutate().then(() => {
-                enqueueSnackbar(t("updated"), {variant: 'success'})
+                enqueueSnackbar(t("updated"), {variant: 'success'});
+                if (typeof newFees.act !== 'string') {
+                    setCreate(false);
+                    setNewFees({act: null, fees: ''});
+                }
             });
         });
-        //setselected(v)
-        //setEdit(true)
     };
 
+    const acts = (httpActSpeciality as HttpResponse)?.data as ActModel[];
     const {t, ready} = useTranslation("settings", {keyPrefix: "actfees"});
     if (!ready) return (<LoadingScreen error button={'loading-error-404-reset'} text={"loading-error"}/>);
 
@@ -236,18 +275,67 @@ function ActFees() {
                     mb={2}>
                     <Typography style={{color: theme.palette.grey[400], fontSize: 10}}>{t('newAct')}</Typography>
                     <Stack direction={"row"} alignItems={"center"} spacing={3}>
-                        <TextField id="outlined-basic"
-                                   value={newFees.name}
-                                   size="small"
-                                   label={t('name')}
-                                   InputProps={{
-                                       style: {width: 450, backgroundColor: "white"}
-                                   }}
-                                   onChange={(ev) => {
-                                       newFees.name = ev.target.value
-                                       setNewFees({...newFees})
-                                   }}
-                                   variant="outlined"/>
+                        <Autocomplete
+                            sx={{width: "44%"}}
+                            size="small"
+                            value={newFees.act}
+                            onChange={(event, newValue) => {
+                                if (typeof newValue === 'string') {
+                                    setNewFees({
+                                        ...newFees,
+                                        act: newValue,
+                                    });
+                                } else if (newValue && newValue.inputValue) {
+                                    // Create a new value from the user input
+                                    setNewFees({
+                                        ...newFees,
+                                        act: newValue.inputValue,
+                                    });
+                                } else {
+                                    setNewFees({
+                                        ...newFees,
+                                        act: newValue as ActModel
+                                    });
+                                }
+                            }}
+                            filterOptions={(options, params) => {
+                                const filtered = filter(options, params);
+                                const {inputValue} = params;
+                                // Suggest the creation of a new value
+                                const isExisting = options.some((option) => inputValue === option.name);
+                                if (inputValue !== '' && !isExisting) {
+                                    filtered.push({
+                                        inputValue,
+                                        name: `${t('add_act')} "${inputValue}"`,
+                                    });
+                                }
+
+                                return filtered;
+                            }}
+                            selectOnFocus
+                            clearOnEscape
+                            handleHomeEndKeys
+                            id="name"
+                            options={acts ? acts : []}
+                            getOptionLabel={(option) => {
+                                // Value selected with enter, right from the input
+                                if (typeof option === 'string') {
+                                    return option;
+                                }
+                                // Add "xxx" option created dynamically
+                                if (option.inputValue) {
+                                    return option.inputValue;
+                                }
+                                // Regular option
+                                return option.name;
+                            }}
+                            renderOption={(props, option) => <li {...props}>{option.name}</li>}
+                            freeSolo
+                            renderInput={(params) => (
+                                <TextField {...params} label={t('placeholder_balance_sheet_name')}/>
+                            )}
+                        />
+
                         <TextField id="outlined-basic"
                                    value={newFees.fees}
                                    type={"number"}
@@ -262,11 +350,15 @@ function ActFees() {
                                        setNewFees({...newFees})
                                    }}
                                    variant="outlined"/>
-                        <Button disabled={newFees.name === '' || newFees.fees === ''} onClick={() => {
-                            saveFees()
-                        }}>{t('save')}</Button>
+                        <Button disabled={newFees.act === null || newFees.fees.length === 0}
+                                onClick={() => {
+                                    if (typeof newFees.act === 'string') {
+                                        saveFees()
+                                    } else {
+                                        setActFees(false, newFees);
+                                    }
+                                }}>{t('save')}</Button>
                     </Stack>
-
                 </Stack>}
 
                 <Otable
