de2fc10238c5a879211c97983fa1ff29a9848bf1
mohamed.zghal <mohamed.zghal@med.tn>
2022-12-23 18:19:08 +0100

    feat: insurance validation

--- src/interfaces/InsurancesModel.d.ts
+++ src/interfaces/InsurancesModel.d.ts
@@ -1,6 +1,7 @@
-interface InsurancesModel{
+interface InsurancesModel {
     insurance_number: string;
     insurance_uuid: string;
     insurance_type: string;
-    expanded: boolean;
+    insurance_social?: InsuranceSocialModel;
+    expand: boolean;
 }
