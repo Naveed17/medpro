c680a2bbbc8ac04b93e702e43b79e425ef965a6b
mohamed.zghal <mohamed.zghal@med.tn>
2023-02-23 11:08:41 +0100

    fix sentry production mode

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -7,7 +7,7 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://4f2f71b876e6440d8cda62ff910d55d8@o1281760.ingest.sentry.io/6488321',
+  dsn: SENTRY_DSN,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
