4948d89013eb81d78e739259fd5145d1c7d318b8
MED <mohamed.zghal@med.tn>
2023-04-12 14:46:45 +0100

    fix: set sentry replay settings fix: set init consultation price by type

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -11,7 +11,7 @@ Sentry.init({
     dsn: SENTRY_DSN,
     // Adjust this value in production, or use tracesSampler for greater control
     tracesSampleRate: 0.1,
-    enabled: !["localhost", "develop", "master"].some(element => PATH_URL.includes(element))
+    enabled: !["develop", "master"].some(element => PATH_URL.includes(element))
     // ...
     // Note: if you want to override the automatic release value, do not set a
     // `release` value here - use the environment variable `SENTRY_RELEASE`, so
