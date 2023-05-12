e7448621e36e7ea570cb420195475d718a48a67f
MED <mohamed.zghal@med.tn>
2023-05-12 15:30:27 +0100

    fix: week picker start day

--- src/features/weekDayPicker/components/weekDayPicker.tsx
+++ src/features/weekDayPicker/components/weekDayPicker.tsx
@@ -4,10 +4,6 @@ import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import WeekDayPickerStyled from './overrides/weekDayPickerStyled';
 import moment, {Moment} from "moment-timezone";
-import {DayPicker} from 'react-day-picker';
-import 'react-day-picker/dist/style.css';
-import {fr} from 'date-fns/locale';
-import {WeekCalendar} from "@features/weekDayPicker/components/weekCalendar";
 
 const months: String[] = [];
 
@@ -37,9 +33,7 @@ function WeekDayPicker({...props}) {
 
     useEffect(() => {
         getMonths();
-        const now = action === "reschedule" ? moment().toDate() :
-            moment().diff(moment().startOf('week'), 'days') > 0 ?
-                moment().toDate() : moment().startOf('week').toDate();
+        const now = moment().startOf('week').toDate();
         let daysOfYear = [];
         for (
             let d = new Date(now.getFullYear(), now.getMonth(), now.getDate());
@@ -51,8 +45,6 @@ function WeekDayPicker({...props}) {
         setDaysOfYear(daysOfYear);
     }, [action]) // eslint-disable-line react-hooks/exhaustive-deps
 
-    console.log(initDate.startOf('week').toDate());
-    console.log(initDate.endOf('week').toDate());
     return (
         <WeekDayPickerStyled>
             <Container>
@@ -81,11 +73,6 @@ function WeekDayPicker({...props}) {
                     </div>
                 </Box>
                 <Divider/>
-                {/*<WeekCalendar
-                    currentDate={initDate.toDate()}
-                    onWeekClick={handleOnWeekPick}
-                    daysInMonth={1}
-                    display={true}/>*/}
                 <Box className="week-days">
                     {daysOfYear.slice(currentWeek[0], currentWeek[1]).map((v, i) => (
                         <Box
