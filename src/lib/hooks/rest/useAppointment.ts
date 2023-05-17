a387bf09ac104fb98d9d70228d535ee0c09e99a5
MED <mohamed.zghal@med.tn>
2023-05-17 18:04:25 +0100

    feat : updateAppointmentStatus hook

--- src/lib/hooks/rest/useAppointment.ts
+++ src/lib/hooks/rest/useAppointment.ts
@@ -1,5 +1,5 @@
-import{useRequestMutation} from "../../axios";
-import {UrlMedicalEntitySuffix} from "../../constants";
+import{useRequestMutation} from "@lib/axios";
+import {UrlMedicalEntitySuffix} from "@lib/constants";
 
 function useAppointment(agenda: AgendaConfigurationModel, medical_entity_uuid: string, accessToken: string, locale: string, start?: string, end?: string) {
 
