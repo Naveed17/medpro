a8d6f95a8c9dc4fc76ce5ec8b9f168461a9928fa
Wassim ayedi <wassim.ayadi@med.tn>
2023-06-08 09:59:21 +0100

    feat: date if end date hidden

--- src/features/dialog/components/lifeStyleDialog/AntecedentWidget.tsx
+++ src/features/dialog/components/lifeStyleDialog/AntecedentWidget.tsx
@@ -1,6 +1,5 @@
 import React, {memo, useState} from "react";
 import dynamic from "next/dynamic";
-import {Button} from "@mui/material";
 
 const FormBuilder: any = dynamic(
     () => import("@formio/react").then((mod: any) => mod.Form),
@@ -10,35 +9,37 @@ const FormBuilder: any = dynamic(
 );
 const AntecedentWidget: any = memo(
     ({src, ...props}: any) => {
-        const {state, list,setState} = props;
+        const {state, list, setState} = props;
         const [value, setValue] = useState(list);
         const getData = () => {
             const res = state.find((i: AntecedentsModel) => i.uuid === value.uuid)
-             return res ? res.response : "";
+            return res ? res.response : "";
         }
         return (
             <>
-            <FormBuilder
-                onChange={(ev: any) => {
-                    let items = state.map((item: AntecedentsModel) => ({...item}));
-                    let item = items.find((i: AntecedentsModel) => i.uuid === value.uuid)
-                    if (item) item.response = JSON.stringify(ev.data);
-                    console.log(items)
-                    setState(items)
-                }}
-                submission={{
-                    data: getData()
-                }}
-                form={{
-                    display: "form",
-                    components: value.values,
-                }}
-            />
+                <FormBuilder
+                    onChange={(ev: any) => {
+                        let items = state.map((item: AntecedentsModel) => ({...item}));
+                        let item = items.find((i: AntecedentsModel) => i.uuid === value.uuid)
+                        if (item) item.response = JSON.stringify(ev.data);
+                        console.log(items)
+                        setState(items)
+                    }}
+                    submission={{
+                        data: getData()
+                    }}
+                    form={{
+                        display: "form",
+                        components: value.values,
+                    }}
+                />
+                {/*
             <Button onClick={()=>{
                 value.values.push({input:true, key: "grossesse2", label: "grossesse1", tableView:true, type: "textfield"})
                 setValue({...value})
             }}>add</Button>
-                </>
+*/}
+            </>
         );
     },
     // NEVER UPDATE
