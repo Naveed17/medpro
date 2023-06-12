3e9648300fa70e79e303199a1a4351a2a95d8af8
MED <mohamed.zghal@med.tn>
2023-06-12 12:11:44 +0100

    fix: user page medical entity hook bug

--- src/pages/dashboard/settings/users/index.tsx
+++ src/pages/dashboard/settings/users/index.tsx
@@ -5,24 +5,35 @@ import {serverSideTranslations} from "next-i18next/serverSideTranslations";
 import {configSelector} from "@features/base";
 import {SubHeader} from "@features/subHeader";
 import {RootStyled} from "@features/toolbar";
-import {Box, Button, Stack, Drawer, DialogContent, Typography, DialogActions, DialogTitle, Dialog, Theme,} from "@mui/material";
+import {
+    Box,
+    Button,
+    Stack,
+    Drawer,
+    DialogContent,
+    Typography,
+    DialogActions,
+    DialogTitle,
+    Dialog,
+    Theme,
+} from "@mui/material";
 import {useTranslation} from "next-i18next";
 import {Otable, resetUser} from "@features/table";
 import {useRouter} from "next/router";
 import {useAppDispatch, useAppSelector} from "@lib/redux/hooks";
 import {NoDataCard} from "@features/card";
-import {useRequest,useRequestMutation} from "@lib/axios";
+import {useRequest, useRequestMutation} from "@lib/axios";
 import {LoadingScreen} from "@features/loadingScreen";
 import IconUrl from "@themes/urlIcon";
 import {AccessMenage} from "@features/drawer";
 import {useMedicalEntitySuffix} from "@lib/hooks";
 import {useSession} from "next-auth/react";
-import { LoadingButton } from "@mui/lab";
+import {LoadingButton} from "@mui/lab";
 import CloseIcon from '@mui/icons-material/Close';
-import { useSnackbar } from "notistack";
+import {useSnackbar} from "notistack";
 import {UserMobileCard} from '@features/card';
-import { DesktopContainer } from "@themes/desktopConainter";
-import { MobileContainer } from "@themes/mobileContainer";
+import {DesktopContainer} from "@themes/desktopConainter";
+import {MobileContainer} from "@themes/mobileContainer";
 
 const CardData = {
     mainIcon: "ic-user",
@@ -88,11 +99,11 @@ function Users() {
     const router = useRouter();
     const dispatch = useAppDispatch();
     const {data: session} = useSession();
-    const urlMedicalEntitySuffix = useMedicalEntitySuffix();
+    const {urlMedicalEntitySuffix} = useMedicalEntitySuffix();
     const {enqueueSnackbar} = useSnackbar();
     const {t, ready} = useTranslation("settings", {keyPrefix: "users.config"});
 
-    const {data: httpUsersResponse,mutate} = useRequest({
+    const {data: httpUsersResponse, mutate} = useRequest({
         method: "GET",
         url: `${urlMedicalEntitySuffix}/mehus/${router.locale}`,
         headers: {
@@ -107,10 +118,10 @@ function Users() {
     const [selected, setSelected] = useState<any>("");
     const {direction} = useAppSelector(configSelector);
     const [open, setOpen] = useState(false);
-    const handleChange = (props: any,event:any) => {
+    const handleChange = (props: any, event: any) => {
         const form = new FormData();
-               form.append("attribute", "isActive");
-               form.append("value", JSON.stringify(event.target.checked));
+        form.append("attribute", "isActive");
+        form.append("value", JSON.stringify(event.target.checked));
         trigger({
             method: "PATCH",
             url: `${urlMedicalEntitySuffix}/edit/user/${props.uuid}/${router.locale}`,
@@ -129,22 +140,22 @@ function Users() {
         setSelected(props);
         setDeleteDialog(true);
     }
-const deleteUser = ()=>{
-    setLoading(true);
-    trigger({
+    const deleteUser = () => {
+        setLoading(true);
+        trigger({
             method: "DELETE",
             url: `${urlMedicalEntitySuffix}/users/${selected.uuid}/${router.locale}`,
             headers: {Authorization: `Bearer ${session?.accessToken}`}
         }).then(() => {
-          enqueueSnackbar(t("delete_success"), {variant: 'success'})  
-          setLoading(false);
-          setDeleteDialog(false);
-          mutate();
+            enqueueSnackbar(t("delete_success"), {variant: 'success'})
+            setLoading(false);
+            setDeleteDialog(false);
+            mutate();
         }).catch(() => {
-          setLoading(false);
+            setLoading(false);
         });
     }
-  
+
     if (!ready)
         return (
             <LoadingScreen
@@ -162,9 +173,9 @@ const deleteUser = ()=>{
                 </RootStyled>
                 <Stack direction="row" alignItems="center" spacing={2}>
                     <Button
-                    sx={{
-                        display: {xs: "none", md: "flex"},
-                    }}
+                        sx={{
+                            display: {xs: "none", md: "flex"},
+                        }}
                         onClick={() => setOpen(true)}
                         startIcon={<IconUrl path="ic-setting"/>}
                         variant="contained">
@@ -185,24 +196,24 @@ const deleteUser = ()=>{
             <Box className="container">
                 {users && users.length > 0 ? (
                     <>
-                    <DesktopContainer>
-                    <Otable
-                        headers={headCells}
-                        rows={users}
-                        from={"users"}
-                        {...{t, handleChange}}
-                        edit={onDelete}
-                    />
-                    </DesktopContainer>
-                    <MobileContainer>
-                        <Stack spacing={1}>
-                        {users.map((user) => (
-                            <React.Fragment key={user.uuid}>
-                            <UserMobileCard data= {user} t={t}/>
-                            </React.Fragment>
-                        ))}
-                        </Stack>
-                    </MobileContainer>
+                        <DesktopContainer>
+                            <Otable
+                                headers={headCells}
+                                rows={users}
+                                from={"users"}
+                                {...{t, handleChange}}
+                                edit={onDelete}
+                            />
+                        </DesktopContainer>
+                        <MobileContainer>
+                            <Stack spacing={1}>
+                                {users.map((user) => (
+                                    <React.Fragment key={user.uuid}>
+                                        <UserMobileCard data={user} t={t}/>
+                                    </React.Fragment>
+                                ))}
+                            </Stack>
+                        </MobileContainer>
                     </>
                 ) : (
                     <NoDataCard t={t} ns={"settings"} data={CardData}/>
@@ -221,42 +232,44 @@ const deleteUser = ()=>{
                 onClose={closeDraw}>
                 <AccessMenage t={t}/>
             </Drawer>
-              <Dialog PaperProps={{sx:{
-        width: "100%"
-      }}} maxWidth="sm" open={deleteDialog}>
-        <DialogTitle sx={{
-          bgcolor: (theme:Theme) => theme.palette.error.main,
-          px:1,
-          py:2,
+            <Dialog PaperProps={{
+                sx: {
+                    width: "100%"
+                }
+            }} maxWidth="sm" open={deleteDialog}>
+                <DialogTitle sx={{
+                    bgcolor: (theme: Theme) => theme.palette.error.main,
+                    px: 1,
+                    py: 2,
 
-        }}>
-         {t("dialog.delete-user-title")}
-        </DialogTitle>
-        <DialogContent style={{paddingTop:20}}>
-          <Typography>
-          {t("dialog.delete-user-desc")}
-          </Typography>
-        </DialogContent>
-        <DialogActions sx={{borderTop:1,borderColor:"divider",px:1 ,py:2}}>
-            <Stack direction="row" spacing={1}>
-            <Button
-              onClick={() => {
-                setDeleteDialog(false);
-              }}
-              startIcon={<CloseIcon />}>
-              {t("dialog.cancel")}
-            </Button>
-            <LoadingButton
-              variant="contained"
-              loading={loading}
-              color="error"
-              onClick={() => deleteUser()}
-              startIcon={<IconUrl path="setting/icdelete" color="white" />}>
-              {t("dialog.delete")}
-            </LoadingButton>
-          </Stack>
-        </DialogActions>
-      </Dialog>
+                }}>
+                    {t("dialog.delete-user-title")}
+                </DialogTitle>
+                <DialogContent style={{paddingTop: 20}}>
+                    <Typography>
+                        {t("dialog.delete-user-desc")}
+                    </Typography>
+                </DialogContent>
+                <DialogActions sx={{borderTop: 1, borderColor: "divider", px: 1, py: 2}}>
+                    <Stack direction="row" spacing={1}>
+                        <Button
+                            onClick={() => {
+                                setDeleteDialog(false);
+                            }}
+                            startIcon={<CloseIcon/>}>
+                            {t("dialog.cancel")}
+                        </Button>
+                        <LoadingButton
+                            variant="contained"
+                            loading={loading}
+                            color="error"
+                            onClick={() => deleteUser()}
+                            startIcon={<IconUrl path="setting/icdelete" color="white"/>}>
+                            {t("dialog.delete")}
+                        </LoadingButton>
+                    </Stack>
+                </DialogActions>
+            </Dialog>
         </>
     );
 }
