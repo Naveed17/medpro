9cbde4b234098304b4757c00a6884bb82700571e
MED <mohamed.zghal@med.tn>
2023-06-21 17:14:07 +0100

    fix: update check notification hook

--- src/lib/hooks/checkNotification.ts
+++ src/lib/hooks/checkNotification.ts
@@ -11,26 +11,17 @@ export const checkNotification = () => {
     if (!Reflect.has(window, 'Notification')) {
         console.log('This browser does not support notifications.');
     } else {
-        if (checkNotificationPromise()) {
-            Notification.requestPermission().then(handlePermission);
-        } else {
+        new Promise((resolve, reject) => {
+            Notification.requestPermission().then(() => resolve(true));
+        }).catch(() => {
             Notification.requestPermission(handlePermission);
-        }
+        }).then(() => {
+            Notification.requestPermission().then(handlePermission);
+        });
+
     }
 
     // At last, if the user has denied notifications, and you
     // want to be respectful there is no need to bother them anymore.
     return Notification?.permission;
-}
-
-// Check whether browser supports the promise version of requestPermission()
-// Safari only supports the old callback-based version
-const checkNotificationPromise = () => {
-    try {
-        Notification.requestPermission().then();
-    } catch (e) {
-        return false;
-    }
-
-    return true;
 }
