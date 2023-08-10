0e9d184a68859a4cfc398ac0fc2b05298c91512c
Wassim ayedi <wassim.ayadi@med.tn>
2023-08-10 10:45:45 +0100

    trans

--- src/pages/dashboard/settings/templates/index.tsx
+++ src/pages/dashboard/settings/templates/index.tsx
@@ -179,6 +179,7 @@ function TemplatesConfig() {
             }
         } as any).then(() => {
             mutatePrescription().then(() => {
+                enqueueSnackbar(t("updated"), {variant: "success"});
                 setOpenDialog(false);
                 setInfo(null);
                 dispatch(SetSelectedDialog(null))
