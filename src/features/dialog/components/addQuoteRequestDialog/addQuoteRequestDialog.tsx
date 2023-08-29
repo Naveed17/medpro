f9b632f2e218ab3bd3f33f90e5eefaffa5a1cbaa
Wassim ayedi <wassim.ayadi@med.tn>
2023-08-29 14:44:47 +0100

    show & create quote

--- src/features/dialog/components/addQuoteRequestDialog/addQuoteRequestDialog.tsx
+++ src/features/dialog/components/addQuoteRequestDialog/addQuoteRequestDialog.tsx
@@ -1,14 +1,9 @@
 import React, {useEffect, useState} from "react";
 import {Button, Stack, Typography} from "@mui/material";
 import AddRequestQuoteDialogStyle from "./overrides/addRequestQuoteDialogStyle";
-
 import {useTranslation} from "next-i18next";
-import {useRequest} from "@lib/axios";
 import {useSession} from "next-auth/react";
-import {useRouter} from "next/router";
 import {FeesTab} from "@features/tabPanel";
-import {SWRNoValidateConfig} from "@lib/swr/swrProvider";
-import {useMedicalEntitySuffix, useMedicalProfessionalSuffix} from "@lib/hooks";
 
 import {Session} from "next-auth";
 import {DefaultCountry} from "@lib/constants";
@@ -17,13 +12,10 @@ function AddRequestQuoteDialog({...props}) {
 
     const {data} = props;
     const {acts, setActs} = data
-    const router = useRouter();
     const {data: session} = useSession();
-    const {urlMedicalEntitySuffix} = useMedicalEntitySuffix();
     const {t} = useTranslation("consultation");
 
     const [total, setTotal] = useState(0);
-    const {medical_professional} = useMedicalProfessionalSuffix();
 
     const {data: user} = session as Session;
 
@@ -32,38 +24,7 @@ function AddRequestQuoteDialog({...props}) {
     const doctor_country = medical_entity.country ? medical_entity.country : DefaultCountry;
     const devise = doctor_country.currency?.name;
 
-    const {data: httpProfessionalsActs, mutate: mutateActs} = useRequest(medical_professional ? {
-        method: "GET",
-        url: `${urlMedicalEntitySuffix}/professionals/${medical_professional?.uuid}/acts/${router.locale}`,
-        headers: {Authorization: `Bearer ${session?.accessToken}`},
-    } : null, SWRNoValidateConfig);
-
-    useEffect(() => {
-        if (httpProfessionalsActs) {
-            let _acts: AppointmentActModel[] = [];
-            (httpProfessionalsActs as HttpResponse)?.data.forEach((act: any) => {
-                const act_index = _acts.findIndex(_act => _act.uuid === act.uuid)
-                if (act_index > -1) {
-                    _acts[act_index] = {
-                        ..._acts[act_index],
-                        selected: false,
-                        qte: act.qte ? act.qte : 1,
-                        fees: act.fees
-                    }
-                } else {
-                    _acts.push({
-                        act: {name: act.act.name},
-                        fees: act.fees,
-                        isTopAct: false,
-                        qte: act.qte ? act.qte : 1,
-                        selected: false,
-                        uuid: act.uuid
-                    })
-                }
-            })
-            setActs(_acts);
-        }
-    }, [httpProfessionalsActs]); // eslint-disable-line react-hooks/exhaustive-deps
+    console.log(acts);
 
     useEffect(() => {
         let _total = 0
--- src/features/tabPanel/components/tabPanels/patient/components/documentsPanel.tsx
+++ src/features/tabPanel/components/tabPanels/patient/components/documentsPanel.tsx
@@ -4,9 +4,12 @@ import {useTranslation} from "next-i18next";
 import {
     AppBar,
     Box,
+    Button,
     CardContent,
     Checkbox,
+    DialogActions,
     FormControlLabel,
+    IconButton,
     LinearProgress,
     Stack,
     Tab,
@@ -22,15 +25,24 @@ import ImageViewer from "react-simple-image-viewer";
 import dynamic from "next/dynamic";
 import PanelCardStyled from "./overrides/panelCardStyled";
 import Icon from "@themes/urlIcon";
-import {a11yProps, useMedicalEntitySuffix} from "@lib/hooks";
+import IconUrl from "@themes/urlIcon";
+import {a11yProps, useMedicalEntitySuffix, useMedicalProfessionalSuffix} from "@lib/hooks";
 import {TabPanel} from "@features/tabPanel";
 import {useAppSelector} from "@lib/redux/hooks";
 import {consultationSelector} from "@features/toolbar";
 import {useRouter} from "next/router";
 import useDocumentsPatient from "@lib/hooks/rest/useDocumentsPatient";
-import {useRequest} from "@lib/axios";
+import {useRequest, useRequestMutation} from "@lib/axios";
 import {SWRNoValidateConfig} from "@lib/swr/swrProvider";
 import {dashLayoutSelector} from "@features/base";
+import CloseIcon from "@mui/icons-material/Close";
+import SaveRoundedIcon from "@mui/icons-material/SaveRounded";
+import moment from "moment/moment";
+import Add from "@mui/icons-material/Add";
+import DocumentCardStyled from "@features/card/components/documentCard/components/overrides/documentCardStyle";
+import EventRoundedIcon from "@mui/icons-material/EventRounded";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import {useSession} from "next-auth/react";
 
 const LoadingScreen = dynamic(() => import('@features/loadingScreen/components/loadingScreen'));
 
@@ -65,10 +77,14 @@ function DocumentsPanel({...props}) {
         loadingRequest, setLoadingRequest
     } = props;
     const router = useRouter();
+    const {data: session} = useSession();
     const {patientDocuments, mutatePatientDocuments} = useDocumentsPatient({patientId: patient?.uuid});
     // translation
     const {t, ready} = useTranslation(["consultation", "patient"]);
     const {selectedDialog} = useAppSelector(consultationSelector);
+
+    const {trigger: triggerQuote} = useRequestMutation(null, "/patient/quote");
+
     // filter checked array
     const [selectedTypes, setSelectedTypes] = useState<string[]>([]);
     const [openDialog, setOpenDialog] = useState<boolean>(false);
@@ -76,6 +92,10 @@ function DocumentsPanel({...props}) {
     const [isViewerOpen, setIsViewerOpen] = useState<string>('');
     const [documents, setDocuments] = useState<any[]>([]);
     const [currentTab, setCurrentTab] = React.useState(documentViewIndex);
+    const [quotes, setQuotes] = useState<any[]>([]);
+    const [openQuoteDialog, setOpenQuoteDialog] = useState<boolean>(false);
+    const [acts, setActs] = useState<AppointmentActModel[]>([]);
+
     const tabsContent = [
         {
             title: "Documents du rendez-vous",
@@ -120,6 +140,7 @@ function DocumentsPanel({...props}) {
                             {patientDocuments?.filter((doc: MedicalDocuments) => doc.documentType === 'photo').map((card: any, idx: number) =>
                                 <React.Fragment key={`doc-item-${idx}`}>
                                     <DocumentCard onClick={() => {
+                                        console.log(card);
                                         showDoc(card)
                                     }} {...{t, data: card, date: false, time: true, title: true, resize: true}}/>
                                 </React.Fragment>
@@ -144,6 +165,7 @@ function DocumentsPanel({...props}) {
                                 <React.Fragment key={`doc-item-${idx}`}>
                                     <DocumentCard
                                         onClick={() => {
+                                            console.log(card);
                                             showDoc(card)
                                         }}
                                         {...{t, data: card, date: true, time: true, title: true}}/>
@@ -161,31 +183,38 @@ function DocumentsPanel({...props}) {
 
     const {urlMedicalEntitySuffix} = useMedicalEntitySuffix();
     const {medicalEntityHasUser} = useAppSelector(dashLayoutSelector);
+    const {medical_professional} = useMedicalProfessionalSuffix();
 
     const {data: httpAppDocPatientResponse} = useRequest(medicalEntityHasUser && patient ? {
         method: "GET",
         url: `${urlMedicalEntitySuffix}/mehu/${medicalEntityHasUser[0].uuid}/patients/${patient.uuid}/appointments/documents/${router.locale}`
     } : null, SWRNoValidateConfig);
 
-    // handle change for checkboxes
-    const handleToggle =
-        (value: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
-            if (e.target.checked) {
-                setSelectedTypes([...selectedTypes, value])
-            } else {
-                selectedTypes.splice(selectedTypes.indexOf(value), 1);
-                setSelectedTypes([...selectedTypes])
-            }
-        };
+    const {data: httpQuotesResponse, mutate: mutateQuotes} = useRequest({
+        method: "GET",
+        url: `${urlMedicalEntitySuffix}/patients/${patient.uuid}/quotes/${router.locale}`
+    });
+    const {data: httpProfessionalsActs} = useRequest(medical_professional ? {
+        method: "GET",
+        url: `${urlMedicalEntitySuffix}/professionals/${medical_professional?.uuid}/acts/${router.locale}`,
+        headers: {Authorization: `Bearer ${session?.accessToken}`},
+    } : null, SWRNoValidateConfig);
 
+    // handle change for checkboxes
+    const handleToggle = (value: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.checked) {
+            setSelectedTypes([...selectedTypes, value])
+        } else {
+            selectedTypes.splice(selectedTypes.indexOf(value), 1);
+            setSelectedTypes([...selectedTypes])
+        }
+    };
     const handleCloseDialog = () => {
         setOpenDialog(false);
     }
-
     const handleTabsChange = (event: React.SyntheticEvent, newValue: number) => {
         setCurrentTab(newValue);
     };
-
     const showDoc = (card: any) => {
         if (card.documentType === 'medical-certificate') {
             setOpenDialog(true);
@@ -241,6 +270,59 @@ function DocumentsPanel({...props}) {
             setOpenDialog(true);
         }
     }
+    const saveQuote = () => {
+        if (medicalEntityHasUser) {
+            let rows: {
+                act_item: string;
+                name_item: string;
+                qty_item: string;
+                price_item: string;
+                discount_item: string;
+                unit_item: string;
+            }[] = [];
+            acts.filter(act => act.selected).map(act => {
+                console.log(act);
+                rows.push({
+                    "act_item": (act.act.uuid as string),
+                    "name_item": act.act.name,
+                    "qty_item": act.qte.toString(),
+                    "price_item": act.fees.toString(),
+                    "discount_item": "",
+                    "unit_item": ""
+                })
+            })
+            const form = new FormData();
+            form.append("patient", patient.uuid);
+            form.append("num_quote", "");
+            form.append("notes", "");
+            form.append("quote_items", JSON.stringify(rows));
+
+            triggerQuote({
+                method: "POST",
+                url: `${urlMedicalEntitySuffix}/mehu/${medicalEntityHasUser[0].uuid}/quotes/${router.locale}`,
+                data: form
+            }).then(() => {
+                mutateQuotes().then(() => {
+                    setOpenQuoteDialog(false)
+                    showQuote(acts.filter(act => act.selected));
+                })
+            });
+        }
+    }
+    const showQuote = (rows: AppointmentActModel[]) => {
+        let type = "";
+        if (!(patient?.birthdate && moment().diff(moment(patient?.birthdate, "DD-MM-YYYY"), 'years') < 18))
+            type = patient?.gender === "F" ? "Mme " : patient?.gender === "U" ? "" : "Mr "
+
+        setDocument({
+            type: "quote",
+            name: "Quote",
+            info: rows,
+            createdAt: moment().format("DD/MM/YYYY"),
+            patient: `${type} ${patient?.firstName} ${patient?.lastName}`,
+        });
+        setOpenDialog(true);
+    }
 
     useEffect(() => {
         if (selectedDialog && !router.asPath.includes('/dashboard/consultation/')) {
@@ -263,10 +345,121 @@ function DocumentsPanel({...props}) {
         }
     }, [httpAppDocPatientResponse]);
 
+    useEffect(() => {
+        if (httpQuotesResponse) {
+            setQuotes((httpQuotesResponse as HttpResponse)?.data)
+        }
+    }, [httpQuotesResponse]) // eslint-disable-line react-hooks/exhaustive-deps
+
+    useEffect(() => {
+        if (httpProfessionalsActs) {
+            let _acts: AppointmentActModel[] = [];
+            (httpProfessionalsActs as HttpResponse)?.data.forEach((act: any) => {
+                const act_index = _acts.findIndex(_act => _act.uuid === act.uuid)
+                if (act_index > -1) {
+                    _acts[act_index] = {
+                        ..._acts[act_index],
+                        selected: false,
+                        qte: act.qte ? act.qte : 1,
+                        fees: act.fees
+                    }
+                } else {
+                    _acts.push({
+                        act: {uuid: act.act.uuid,name: act.act.name},
+                        fees: act.fees,
+                        isTopAct: false,
+                        qte: act.qte ? act.qte : 1,
+                        selected: false,
+                        uuid: act.uuid
+                    })
+                }
+            })
+            console.log()
+            setActs(_acts);
+        }
+    }, [httpProfessionalsActs]); // eslint-disable-line react-hooks/exhaustive-deps
+
     if (!ready) return (<LoadingScreen color={"error"} button text={"loading-error"}/>);
 
     return (
         <>
+            <PanelCardStyled
+                sx={{
+                    "& .MuiCardContent-root": {
+                        background: "white"
+                    },
+                    "& .injected-svg": {
+                        //maxWidth: 30,
+                        //maxHeight: 30
+                    },
+                    marginBottom: "1rem"
+                }}
+            >
+                <CardContent>
+                    <AppBar position="static" color={"transparent"} className={"app-bar-header"}>
+                        <Toolbar variant="dense">
+                            <Stack direction={"row"}
+                                   alignItems={"center"}
+                                   style={{width: "100%"}}
+                                   justifyContent={"space-between"}>
+                                <Typography
+                                    variant="body1"
+                                    sx={{fontWeight: "bold"}}
+                                    gutterBottom>
+                                    {t("config.tabs.quotes", {ns: 'patient'})}
+                                </Typography>
+                                <IconButton onClick={() => {
+                                    setOpenQuoteDialog(true)
+                                }}><Add/></IconButton>
+                            </Stack>
+                        </Toolbar>
+                    </AppBar>
+
+                    <Box style={{overflowX: "auto", marginBottom: 10}}>
+                        <Stack direction={"row"} spacing={1} m={1} alignItems={"center"}>
+                            {
+                                quotes.map((card: any, idx: number) =>
+                                    <React.Fragment key={`doc-item-${idx}`}>
+                                        <DocumentCardStyled>
+                                            <Stack direction={"row"} spacing={1} onClick={() => {
+                                                console.log(card.quotes_items)
+                                                console.log(acts);
+                                                let _acts: any[] = [];
+                                                acts.map(act => _acts = [..._acts, {...act, selected: card.quotes_items.findIndex((qi: { act_item: { uuid: string; }; }) => qi.act_item.uuid === act.act.uuid) !==-1}])
+                                                showQuote(_acts.filter(act=>act.selected))
+                                            }} alignItems={"center"}
+                                                   padding={2}>
+                                                <IconUrl width={20} path={"ic-text"}/>
+                                                <Stack>
+                                                    <Typography>{t("config.tabs.quotes", {ns: 'patient'})}</Typography>
+                                                    <Stack direction={"row"} spacing={1}>
+                                                        <EventRoundedIcon
+                                                            style={{fontSize: 15, color: "grey"}}/>
+                                                        <Typography whiteSpace={"nowrap"} fontSize={12}
+                                                                    sx={{color: "grey", cursor: "pointer"}}>
+                                                            {moment(card.date_quote, 'DD-MM-YYYY HH:mm').format('DD-MM-YYYY')}
+                                                        </Typography>
+
+                                                        <AccessTimeIcon style={{fontSize: 15, color: "grey"}}/>
+                                                        <Typography whiteSpace={"nowrap"} fontSize={12}
+                                                                    sx={{
+                                                                        marginTop: 0,
+                                                                        color: "grey",
+                                                                        cursor: "pointer"
+                                                                    }}>
+                                                            {moment(card.date_quote, 'DD-MM-YYYY HH:mm').add(1, "hour").format('HH:mm')}
+                                                        </Typography>
+                                                    </Stack>
+                                                </Stack>
+                                            </Stack>
+                                        </DocumentCardStyled>
+                                    </React.Fragment>
+                                )
+                            }
+                        </Stack>
+                    </Box>
+                </CardContent>
+            </PanelCardStyled>
             {documents.length > 0 || patientDocuments?.length > 0 ? (
                 <>
                     {documents.filter((doc: MedicalDocuments) => doc.documentType === 'photo').length > 0 && !roles.includes("ROLE_SECRETARY") &&
@@ -302,6 +495,7 @@ function DocumentsPanel({...props}) {
                                             documents.filter((doc: MedicalDocuments) => doc.documentType === 'photo').map((card: any, idx: number) =>
                                                 <React.Fragment key={`doc-item-${idx}`}>
                                                     <DocumentCard onClick={() => {
+                                                        console.log(card);
                                                         showDoc(card)
                                                     }} {...{
                                                         t,
@@ -344,8 +538,6 @@ function DocumentsPanel({...props}) {
                                     </Box>
                                 </Toolbar>
                             </AppBar>
-
-
                             <>
                                 <FormControlLabel
                                     key={uniqueId()}
@@ -428,6 +620,43 @@ function DocumentsPanel({...props}) {
                     onClose={() => setIsViewerOpen('')}
                 />
             )}
+
+            <Dialog
+                action={"quote-request-dialog"}
+                data={{
+                    acts, setActs
+                }}
+                open={openQuoteDialog}
+                size={"md"}
+                direction={"ltr"}
+                sx={{minHeight: 400}}
+                title={t("add_quote")}
+                dialogClose={() => {
+                    setOpenQuoteDialog(false)
+                }}
+                onClose={() => {
+                    setOpenQuoteDialog(false)
+                }}
+                actionDialog={
+                    <DialogActions>
+                        <Button
+                            onClick={() => {
+                                setOpenQuoteDialog(false)
+                            }}
+                            startIcon={<CloseIcon/>}>
+                            {t("cancel")}
+                        </Button>
+                        <Button
+                            onClick={() => {
+                                saveQuote()
+                            }}
+                            disabled={acts.filter(act => act.selected).length === 0}
+                            startIcon={<SaveRoundedIcon/>}>
+                            {t("save")}
+                        </Button>
+                    </DialogActions>
+                }
+            />
         </>
     );
 }
