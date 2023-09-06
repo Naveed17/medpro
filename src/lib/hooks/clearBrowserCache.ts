6edcca433267b1669fedcc40f1f109a7ef515958
MED <mohamed.zghal@med.tn>
2023-09-06 16:19:14 +0100

    feat: clear server cookies

--- src/lib/hooks/clearBrowserCache.ts
+++ src/lib/hooks/clearBrowserCache.ts
@@ -1,8 +1,16 @@
-export function clearBrowserCache() {
+import axios from "axios";
+
+export async function clearBrowserCache() {
+    console.log("clearBrowserCache");
     // Clearing the session storage
     sessionStorage.clear();
     // Clearing the browser storage
     localStorage.clear();
     // Clearing the browser cache
     caches.keys().then((keyList) => Promise.all(keyList.map((key) => caches.delete(key))));
+    // Clearing the server cookies
+    await axios({
+        url: "/api/helper/clearCookies",
+        method: "GET"
+    });
 }
