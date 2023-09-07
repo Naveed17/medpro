2f5e7660000af0fb03b21c9746e1e01bcedd3010
MED <mohamed.zghal@med.tn>
2023-09-07 17:46:55 +0100

    fix: multiple exports issue

--- src/features/leftActionBar/components/index.ts
+++ src/features/leftActionBar/components/index.ts
@@ -1,6 +1,6 @@
-export { default as LeftActionBar } from './leftActionBar';
-export { default as SettingBarStyled } from './overrides/settingBarStyled';
-export { default as LeftActionsData } from './config';
+export {default as LeftActionBar} from './leftActionBar';
+export {default as SettingBarStyled} from './overrides/settingBarStyled';
+export {default as LeftActionsData} from './config';
 export * from './agenda';
 export * from './settings';
 export * from "./patient";
