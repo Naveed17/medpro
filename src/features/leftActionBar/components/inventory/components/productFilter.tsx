b78f3df0586c1a62d82505a0bed95d8ad22277a0
MED <mohamed.zghal@med.tn>
2023-09-20 17:14:01 +0100

    fix: inventory filter

--- src/features/leftActionBar/components/inventory/components/productFilter.tsx
+++ src/features/leftActionBar/components/inventory/components/productFilter.tsx
@@ -1,161 +1,188 @@
-import React, { useEffect } from "react";
+import React, {useCallback, useEffect} from "react";
 import AutoCompleteMulti from "./autoCompleteMulti";
 import {
-  Stack,
-  Typography,
-  TextField,
-  FormControlLabel,
-  Checkbox,
+    Stack,
+    Typography,
+    TextField,
+    FormControlLabel,
+    Checkbox,
 } from "@mui/material";
-import { FormikProvider, useFormik, Form } from "formik";
+import {FormikProvider, useFormik, Form} from "formik";
 import * as data from "./data";
 import {
-  setFilter,
-  leftActionBarSelector,
-  setFilterData,
+    leftActionBarSelector, setFilter
 } from "@features/leftActionBar";
-import { useAppDispatch, useAppSelector } from "@lib/redux/hooks";
+import {useAppDispatch, useAppSelector} from "@lib/redux/hooks";
+import _ from "lodash";
 
-function ProductFilter({ ...props }) {
-  const { t, OnSearch } = props;
-  const dispatch = useAppDispatch();
-  const { filter } = useAppSelector(leftActionBarSelector);
-  const formik = useFormik({
-    enableReinitialize: true,
-    initialValues: {
-      name: (filter as any)?.name || "",
-      brand: (filter as any)?.brand || [],
-      categories: (filter as any)?.categories || [],
-      stock: (filter as any)?.stock || [],
-      isHidden: (filter as any)?.isHidden || false,
-      isForAppointment: (filter as any)?.isForAppointment || false,
-    },
+function ProductFilter({...props}) {
+    const {t, OnSearch} = props;
+    const dispatch = useAppDispatch();
 
-    onSubmit: (values) => {
-      console.log(values);
-    },
-  });
+    const {query: filter} = useAppSelector(leftActionBarSelector);
 
-  const { setFieldValue, values } = formik;
-  useEffect(() => {
-    const data = {
-      ...values,
-      brand: values.brand.map((item: any) => item?.name),
-      categories: values.categories.map((item: any) => item?.name),
-      stock: values.stock.map((item: any) => item?.name),
-    };
-    OnSearch(data);
-  }, [values]);
-  return (
-    <FormikProvider value={formik}>
-      <Stack spacing={1} component={Form} noValidate>
-        <Stack spacing={0.5}>
-          <Typography>{t("name")}</Typography>
-          <TextField
-            placeholder={t("placeholder_name")}
-            value={values.name}
-            onChange={(e) => {
-              setFieldValue("name", e.target.value);
-              dispatch(
-                setFilterData({
-                  ...filter,
-                  name: e.target.value,
-                })
-              );
-            }}
-          />
-        </Stack>
-        <Stack spacing={0.5}>
-          <Typography>{t("brand")}</Typography>
-          <AutoCompleteMulti
-            t={t}
-            placeholder={t("placeholder_brand")}
-            data={data.BrandsName}
-            onChange={(value: any) => {
-              setFieldValue("brand", value);
-              dispatch(
-                setFilterData({
-                  ...filter,
-                  brand: value,
-                })
-              );
-            }}
-            value={values.brand}
-          />
-        </Stack>
-        <Stack spacing={0.5}>
-          <Typography>{t("category")}</Typography>
-          <AutoCompleteMulti
-            t={t}
-            placeholder={t("placeholder_category")}
-            data={data.CategoriesName}
-            onChange={(value: any) => {
-              setFieldValue("categories", value);
-              dispatch(
-                setFilterData({
-                  ...filter,
-                  categories: value,
-                })
-              );
-            }}
-            value={values.categories}
-          />
-        </Stack>
-        <Stack spacing={0.5}>
-          <Typography>{t("stock")}</Typography>
-          <AutoCompleteMulti
-            t={t}
-            placeholder={t("placeholder_stock")}
-            data={data.ProductsName}
-            onChange={(value: any) => {
-              setFieldValue("stock", value);
-              dispatch(
-                setFilterData({
-                  ...filter,
-                  stock: value,
-                })
-              );
-            }}
-            value={values.stock}
-          />
-        </Stack>
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={values.isHidden}
-              onChange={(e) => {
-                setFieldValue("isHidden", e.target.checked);
-                dispatch(
-                  setFilterData({
-                    ...filter,
-                    isHidden: e.target.checked,
-                  })
-                );
-              }}
-            />
-          }
-          label={t("is_hidden")}
-        />
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={values.isForAppointment}
-              onChange={(e) => {
-                setFieldValue("isForAppointment", e.target.checked);
-                dispatch(
-                  setFilterData({
-                    ...filter,
-                    isForAppointment: e.target.checked,
-                  })
-                );
-              }}
-            />
-          }
-          label={t("is_for_appointment")}
-        />
-      </Stack>
-    </FormikProvider>
-  );
+    const formik = useFormik({
+        enableReinitialize: true,
+        initialValues: {
+            name: filter?.inventory?.name || "",
+            brand: filter?.inventory?.brand || [],
+            categories: filter?.inventory?.categories || [],
+            stock: filter?.inventory?.stock || [],
+            isHidden: filter?.inventory?.isHidden || false,
+            isForAppointment: filter?.inventory?.isForAppointment || false,
+        },
+        onSubmit: (values) => {
+            console.log(values);
+        },
+    });
+
+    const onChangeInput = useCallback((value: any) => {
+        OnSearch(value);
+    }, [OnSearch]);
+
+    const {setFieldValue, values} = formik;
+
+    useEffect(() => {
+        const data = {
+            ...values,
+            brand: values.brand.map((item: any) => item?.name),
+            categories: values.categories.map((item: any) => item?.name),
+            stock: values.stock.map((item: any) => item?.name),
+        };
+        onChangeInput(data);
+    }, [values]); // eslint-disable-line react-hooks/exhaustive-deps
+
+    return (
+        <FormikProvider value={formik}>
+            <Stack spacing={1} component={Form} noValidate>
+                <Stack spacing={0.5}>
+                    <Typography>{t("name")}</Typography>
+                    <TextField
+                        placeholder={t("placeholder_name")}
+                        value={values.name}
+                        onChange={(e) => {
+                            setFieldValue("name", e.target.value);
+                            if (e.target.value.length > 0) {
+                                dispatch(setFilter({
+                                        inventory: {
+                                            ...filter?.inventory,
+                                            name: e.target.value,
+                                        }
+                                    })
+                                );
+                            } else {
+                                const query = _.omit(filter?.inventory, "name");
+                                dispatch(setFilter({inventory: query}));
+                            }
+                        }}
+                    />
+                </Stack>
+                <Stack spacing={0.5}>
+                    <Typography>{t("brand")}</Typography>
+                    <AutoCompleteMulti
+                        t={t}
+                        placeholder={t("placeholder_brand")}
+                        data={data.BrandsName}
+                        onChange={(value: any) => {
+                            setFieldValue("brand", value);
+                            if (value.length > 0) {
+                                dispatch(setFilter({
+                                    inventory: {
+                                        ...filter?.inventory,
+                                        brand: value,
+                                    }
+                                }));
+                            } else {
+                                const query = _.omit(filter?.inventory, "brand");
+                                dispatch(setFilter({inventory: query}));
+                            }
+                        }}
+                        value={values.brand}
+                    />
+                </Stack>
+                <Stack spacing={0.5}>
+                    <Typography>{t("category")}</Typography>
+                    <AutoCompleteMulti
+                        t={t}
+                        placeholder={t("placeholder_category")}
+                        data={data.CategoriesName}
+                        onChange={(value: any) => {
+                            setFieldValue("categories", value);
+                            if (value.length > 0) {
+                                dispatch(setFilter({
+                                    inventory: {
+                                        ...filter?.inventory,
+                                        categories: value
+                                    }
+                                }));
+                            } else {
+                                const query = _.omit(filter?.inventory, "categories");
+                                dispatch(setFilter({inventory: query}));
+                            }
+                        }}
+                        value={values.categories}
+                    />
+                </Stack>
+                <Stack spacing={0.5}>
+                    <Typography>{t("stock")}</Typography>
+                    <AutoCompleteMulti
+                        t={t}
+                        placeholder={t("placeholder_stock")}
+                        data={data.ProductsName}
+                        onChange={(value: any) => {
+                            setFieldValue("stock", value);
+                            if (value.length > 0) {
+                                dispatch(setFilter({
+                                    inventory: {
+                                        ...filter?.inventory,
+                                        stock: value
+                                    }
+                                }));
+                            } else {
+                                const query = _.omit(filter?.inventory, "stock");
+                                dispatch(setFilter({inventory: query}));
+                            }
+                        }}
+                        value={values.stock}
+                    />
+                </Stack>
+                <FormControlLabel
+                    control={
+                        <Checkbox
+                            checked={values.isHidden}
+                            onChange={(e) => {
+                                setFieldValue("isHidden", e.target.checked);
+                                dispatch(setFilter({
+                                    inventory: {
+                                        ...filter?.inventory,
+                                        isHidden: e.target.checked
+                                    }
+                                }));
+                            }}
+                        />
+                    }
+                    label={t("is_hidden")}
+                />
+                <FormControlLabel
+                    control={
+                        <Checkbox
+                            checked={values.isForAppointment}
+                            onChange={(e) => {
+                                setFieldValue("isForAppointment", e.target.checked);
+                                dispatch(setFilter({
+                                    inventory: {
+                                        ...filter?.inventory,
+                                        isForAppointment: e.target.checked,
+                                    }
+                                }));
+                            }}
+                        />
+                    }
+                    label={t("is_for_appointment")}
+                />
+            </Stack>
+        </FormikProvider>
+    );
 }
 
 export default ProductFilter;
--- src/features/leftActionBar/reducer.ts
+++ src/features/leftActionBar/reducer.ts
@@ -1,17 +1,14 @@
-import { createReducer } from "@reduxjs/toolkit";
+import {createReducer} from "@reduxjs/toolkit";
 import {
-  setFilter,
-  resetFilterPatient,
-  resetFilterPayment,
-  setFilterPayment,
-  setFilterData,
+    setFilter,
+    resetFilterPatient,
+    resetFilterPayment,
+    setFilterPayment
 } from "./actions";
 import _ from "lodash";
 
 export type ActionBarState = {
-  filter: any;
-  query:
-    | {
+    query: {
         type?: string;
         reasons?: string;
         status?: string;
@@ -27,40 +24,39 @@ export type ActionBarState = {
             insurances?: string[];
         },
         payment?: {
-          insurance?: string[];
-          dates?: any;
+            insurance?: string[];
+            dates?: any;
+        },
+        inventory?: {
+            name?: string;
+            brand?: any[];
+            categories?: any[];
+            stock?: any[];
+            isHidden?: boolean;
+            isForAppointment?: boolean;
         };
-      }
-    | undefined;
+    } | undefined;
 };
 
 const initialState: ActionBarState = {
-  filter: null,
-  query: undefined,
+    query: undefined
 };
 
 export const leftActionBarReducer = createReducer(initialState, (builder) => {
-  builder
-    .addCase(setFilter, (state, action) => {
-      return { ...state, query: { ...state.query, ...action.payload } };
-    })
-    .addCase(setFilterPayment, (state, action) => {
-      return {
-        ...state,
-        query: {
-          ...state.query,
-          payment: { ...state.query?.payment, ...action.payload },
-        },
-      };
-    })
-    .addCase(resetFilterPatient, (state) => {
-      const queryState: any = _.omit(state.query, ["acts", "disease"]);
-      return { ...state, query: { ...queryState, patient: undefined } };
-    })
-    .addCase(resetFilterPayment, (state) => {
-      return { ...state, query: { ...state.query, payment: undefined } };
-    })
-    .addCase(setFilterData, (state, action) => {
-      return { ...state, filter: action.payload };
+    builder.addCase(setFilter, (state, action) => {
+        return {...state, query: {...state.query, ...action.payload}};
+    }).addCase(setFilterPayment, (state, action) => {
+        return {
+            ...state,
+            query: {
+                ...state.query,
+                payment: {...state.query?.payment, ...action.payload},
+            },
+        };
+    }).addCase(resetFilterPatient, (state) => {
+        const queryState: any = _.omit(state.query, ["acts", "disease"]);
+        return {...state, query: {...queryState, patient: undefined}};
+    }).addCase(resetFilterPayment, (state) => {
+        return {...state, query: {...state.query, payment: undefined}};
     });
 });
