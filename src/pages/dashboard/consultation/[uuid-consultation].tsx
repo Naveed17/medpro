d44086ce35fde6e3e40f7cbafdfc7e11ac9c05ba
Wassim ayedi <wassim.ayadi@med.tn>
2023-10-04 09:49:53 +0100

    fix obs

--- src/pages/dashboard/consultation/[uuid-consultation].tsx
+++ src/pages/dashboard/consultation/[uuid-consultation].tsx
@@ -164,7 +164,6 @@ function ConsultationInProgress() {
     const [isViewerOpen, setIsViewerOpen] = useState<string>("");
     const [transactions, setTransactions] = useState(null);
     const [restAmount, setRestAmount] = useState(0);
-    const [switchTab, setSwitchTab] = useState(false);
 
     const handleChangeTab = (_: React.SyntheticEvent, newValue: string) => {
         setSelectedTab(newValue)
@@ -603,13 +602,10 @@ function ConsultationInProgress() {
     }, [tableState.patientId]);
 
     useEffect(() => {
-        if (switchTab) {
             setLoading(true)
             mutateSheetData().then(() => {
                 setLoading(false)
             })
-        }
-        setSwitchTab(true)
     }, [selectedTab]) // eslint-disable-line react-hooks/exhaustive-deps
 
     useEffect(() => {
