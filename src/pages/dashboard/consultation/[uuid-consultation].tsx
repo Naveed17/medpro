cca15f454f04725f79b7742ab925840c09873ab3
MED <mohamed.zghal@med.tn>
2023-10-10 17:14:03 +0100

    fix: search patient pagination issue

--- src/pages/dashboard/consultation/[uuid-consultation].tsx
+++ src/pages/dashboard/consultation/[uuid-consultation].tsx
@@ -411,50 +411,6 @@ function ConsultationInProgress() {
         return payed_amount;
     }
 
-    const sendNotification = () => {
-        triggerUsers({
-            method: "GET",
-            url: `${urlMedicalEntitySuffix}/users`
-        }, {
-            onSuccess: (r: any) => {
-                const secretary = (r?.data as HttpResponse).data;
-                if (secretary.length > 0 && patient) {
-                    const localInstr = localStorage.getItem(`instruction-data-${app_uuid}`);
-                    const restAmount = getTransactionAmountPayed();
-                    const form = new FormData();
-                    form.append("action", "end_consultation");
-                    form.append("root", "agenda");
-                    form.append("content",
-                        JSON.stringify({
-                            fees: total,
-                            restAmount: total - restAmount,
-                            instruction: localInstr ? localInstr : "",
-                            control: checkedNext,
-                            edited: false,
-                            payed: transactions ? restAmount === 0 : restAmount !== 0,
-                            nextApp: meeting ? meeting : "0",
-                            appUuid: app_uuid,
-                            dayDate: sheet?.date,
-                            patient: {
-                                uuid: patient.uuid,
-                                email: patient.email,
-                                birthdate: patient.birthdate,
-                                firstName: patient.firstName,
-                                lastName: patient.lastName,
-                                gender: patient.gender,
-                            },
-                        })
-                    );
-                    triggerNotificationPush({
-                        method: "POST",
-                        url: `${urlMedicalEntitySuffix}/professionals/notification/${router.locale}`,
-                        data: form
-                    });
-                }
-            }
-        })
-    }
-
     const checkTransactions = () => {
         if (!transactions && app_uuid) {
             const form = new FormData();
@@ -481,27 +437,25 @@ function ConsultationInProgress() {
         form.append("status", "5");
         form.append("action", "end_consultation");
         form.append("root", "agenda");
-        form.append("content",
-            JSON.stringify({
-                fees: total,
-                restAmount: total - restAmount,
-                instruction: localInstr ? localInstr : "",
-                control: checkedNext,
-                edited: false,
-                payed: transactions ? restAmount === 0 : restAmount !== 0,
-                nextApp: meeting ? meeting : "0",
-                appUuid: app_uuid,
-                dayDate: sheet?.date,
-                patient: {
-                    uuid: patient?.uuid,
-                    email: patient?.email,
-                    birthdate: patient?.birthdate,
-                    firstName: patient?.firstName,
-                    lastName: patient?.lastName,
-                    gender: patient?.gender,
-                },
-            })
-        );
+        form.append("content", JSON.stringify({
+            fees: total,
+            restAmount: total - restAmount,
+            instruction: localInstr ? localInstr : "",
+            control: checkedNext,
+            edited: false,
+            payed: transactions ? restAmount === 0 : restAmount !== 0,
+            nextApp: meeting ? meeting : "0",
+            appUuid: app_uuid,
+            dayDate: sheet?.date,
+            patient: {
+                uuid: patient?.uuid,
+                email: patient?.email,
+                birthdate: patient?.birthdate,
+                firstName: patient?.firstName,
+                lastName: patient?.lastName,
+                gender: patient?.gender,
+            },
+        }));
         triggerAppointmentEdit({
             method: "PUT",
             url: `${urlMedicalEntitySuffix}/agendas/${agenda?.uuid}/appointments/${app_uuid}/data/${router.locale}`,
@@ -513,7 +467,6 @@ function ConsultationInProgress() {
                     dispatch(resetTimer());
                     dispatch(openDrawer({type: "view", open: false}));
                 });
-                //sendNotification();
                 checkTransactions();
                 clearData();
                 mutateOnGoing();
