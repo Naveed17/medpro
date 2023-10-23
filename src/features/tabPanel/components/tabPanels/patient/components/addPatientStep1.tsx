55470f6c5d3efc2b33cdce6ce05b04d0c370e987
MED <mohamed.zghal@med.tn>
2023-10-23 15:39:59 +0100

    fix: add patient first/last name validation regex

--- src/features/tabPanel/components/tabPanels/patient/components/addPatientStep1.tsx
+++ src/features/tabPanel/components/tabPanels/patient/components/addPatientStep1.tsx
@@ -86,12 +86,12 @@ function AddPatientStep1({...props}) {
         first_name: Yup.string()
             .min(3, t("first-name-error"))
             .max(50, t("first-name-error"))
-            .matches(/^[aA-zZ\s]+$/, t("special-text-error"))
+            .matches(/^[aA-zZء-ي\s]+$/, t("special-text-error"))
             .required(t("first-name-error")),
         last_name: Yup.string()
             .min(3, t("last-name-error"))
             .max(50, t("last-name-error"))
-            .matches(/^[aA-zZ\s]+$/, t("special-text-error"))
+            .matches(/^[aA-zZء-ي\s]+$/, t("special-text-error"))
             .required(t("last-name-error")),
         phones: Yup.array().of(
             Yup.object().shape({
