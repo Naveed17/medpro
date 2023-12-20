4eda99c16608e0554004f2876599800e2d7e7b3a
MED <mohamed.zghal@med.tn>
2023-12-20 10:52:29 +0100

    fix: sort waiting room SR fix: fullscreen button actions

--- src/features/buttons/components/minMaxWindowButton/components/overrides/buttonStyle.ts
+++ src/features/buttons/components/minMaxWindowButton/components/overrides/buttonStyle.ts
@@ -1,14 +1,18 @@
-import { IconButton,styled,IconButtonProps, SxProps, Theme } from "@mui/material";
-interface ButtonProps extends IconButtonProps{
+import {IconButton, styled, IconButtonProps, SxProps, Theme} from "@mui/material";
+
+interface ButtonProps extends IconButtonProps {
     sx?: SxProps<Theme>
 }
-const ButtonSyled = styled(IconButton)<ButtonProps>(({theme})=>({
-    backgroundColor:theme.palette.grey["A500"],
-    border:`1px solid ${theme.palette.divider}`,
-    borderRadius:theme.shape.borderRadius,
-    padding:theme.spacing(1.2),
-    '.react-svg div':{
-        display:'flex'
+
+const ButtonSyled = styled(IconButton)<ButtonProps>(({theme}) => ({
+    backgroundColor: theme.palette.grey["A500"],
+    width: 37,
+    height: 37,
+    border: `1px solid ${theme.palette.divider}`,
+    borderRadius: theme.shape.borderRadius,
+    padding: theme.spacing(1.2),
+    '.react-svg div': {
+        display: 'flex'
     }
 }));
 
--- src/features/toolbar/components/roomToolbar/roomToolbar.tsx
+++ src/features/toolbar/components/roomToolbar/roomToolbar.tsx
@@ -1,15 +1,36 @@
-import {Stack, useMediaQuery, Button, Tabs, Tab} from '@mui/material'
+import {Stack, useMediaQuery, Button, Tabs, Tab, Badge, Avatar} from '@mui/material'
 import {DrawerBottom} from '@features/drawerBottom';
 import {WaitingRoom} from '@features/leftActionBar'
 import Icon from '@themes/urlIcon'
 import React, {SyntheticEvent, useCallback, useState} from 'react';
 import {MobileContainer} from '@themes/mobileContainer';
 import {a11yProps} from "@lib/hooks";
-import { MinMaxWindowButton } from '@features/buttons';
+import {MinMaxWindowButton, minMaxWindowSelector} from '@features/buttons';
+import {LoadingButton} from "@mui/lab";
+import IconUrl from "@themes/urlIcon";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
+import {CipCard} from "@features/card";
+import {useAppDispatch, useAppSelector} from "@lib/redux/hooks";
+import {onOpenPatientDrawer} from "@features/table";
 
 function RoomToolbar({...props}) {
-    const {t, tabIndex, setTabIndex, columns, data, handleCollapse} = props;
+    const {
+        t,
+        tabIndex,
+        setTabIndex,
+        setPatientDetailDrawer,
+        nextConsultation,
+        columns,
+        data,
+        handleCollapse,
+        is_next,
+        isActive
+    } = props;
     const isMobile = useMediaQuery((theme: any) => theme.breakpoints.down('sm'));
+    const dispatch = useAppDispatch();
+
+    const {isWindowMax} = useAppSelector(minMaxWindowSelector);
+
     const [open, set0pen] = useState(false);
     const [tabsContent] = useState([
         {
@@ -27,11 +48,12 @@ function RoomToolbar({...props}) {
     }, [setTabIndex]);
 
     return (
-        <Stack sx={{
-            ".tabs-bg-white.tabs-bg-white": {
-                borderTopWidth: 0,
-            }
-        }} direction='row' justifyContent="space-between" mt={2.8} width={1} alignItems="center">
+        <Stack
+            sx={{
+                ".tabs-bg-white.tabs-bg-white": {
+                    borderTopWidth: 0,
+                }
+            }} direction='row' justifyContent="space-between" mt={2.8} width={1} alignItems="center">
             <Tabs
                 value={tabIndex}
                 onChange={handleStepperIndexChange}
@@ -88,8 +110,7 @@ function RoomToolbar({...props}) {
                         left: '50%',
                         zIndex: 999,
 
-                    }}
-                >
+                    }}>
                     Filtrer (0)
                 </Button>
             </MobileContainer>
@@ -99,8 +120,87 @@ function RoomToolbar({...props}) {
                 title="Filter">
                 <WaitingRoom/>
             </DrawerBottom>
-            
-            <MinMaxWindowButton sx={{alignSelf:'flex-start'}} />
+
+            <Stack direction={"row"} alignItems={"center"} mb={.5}>
+                {(is_next && isWindowMax) &&
+                    <LoadingButton
+                        disableRipple
+                        color={"black"}
+                        onClick={() => {
+                            dispatch(onOpenPatientDrawer({patientId: is_next.patient_uuid}));
+                            setPatientDetailDrawer(true);
+                        }}
+                        sx={{
+                            scale: "0.96",
+                            mr: isActive ? 0 : 1,
+                            p: "6px 12px",
+                            backgroundColor: (theme) => theme.palette.info.lighter,
+                            '&:hover': {
+                                backgroundColor: (theme) => theme.palette.info.lighter,
+                            }
+                        }}
+                        loadingPosition={"start"}
+                        startIcon={<Badge
+                            overlap="circular"
+                            anchorOrigin={{vertical: 'bottom', horizontal: 'right'}}
+                            badgeContent={
+                                <Avatar alt="Small avatar" sx={{
+                                    pt: .2,
+                                    width: 16,
+                                    height: 16,
+                                    borderRadius: 20,
+                                    border: theme => `2px solid ${theme.palette.background.paper}`
+                                }}>
+                                    <IconUrl width={14} height={16} path={"ic-next"}/>
+                                </Avatar>
+                            }>
+                            <Avatar
+                                sx={{
+                                    width: 30,
+                                    height: 30,
+                                    borderRadius: 20,
+                                    border: theme => `2px solid ${theme.palette.background.paper}`
+                                }} variant={"circular"}
+                                src={`/static/icons/men-avatar.svg`}/>
+                        </Badge>}
+                        variant={"contained"}>
+                        <Stack direction={"row"} alignItems={"center"}>
+                            {is_next.patient}
+                            <Avatar
+                                alt="Small avatar"
+                                variant={"square"}
+                                onClick={(event) => {
+                                    event.stopPropagation();
+                                    nextConsultation({is_next: true, uuid: is_next.uuid});
+                                }}
+                                sx={{
+                                    ml: 1,
+                                    background: "#FFF",
+                                    width: 30,
+                                    height: 30,
+                                    border: theme => `1px solid ${theme.palette.grey["A900"]}`
+                                }}>
+                                <CloseRoundedIcon
+                                    sx={{
+                                        color: theme => theme.palette.text.primary,
+                                        width: 20,
+                                        height: 20
+                                    }}/>
+                            </Avatar>
+                        </Stack>
+                    </LoadingButton>
+                }
+
+                {(isActive && isWindowMax) &&
+                    <CipCard
+                        openPatientDialog={(uuid: string) => {
+                            dispatch(onOpenPatientDrawer({patientId: uuid}));
+                            setPatientDetailDrawer(true);
+                        }}/>
+                }
+                <MinMaxWindowButton/>
+            </Stack>
+
         </Stack>
     )
 }
