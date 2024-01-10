7ac0195e7ab4b2d395df5c336d1e45dbd4fb6b1f
MED <mohamed.zghal@med.tn>
2024-01-10 12:03:11 +0100

    fix: status chart issue

--- src/pages/dashboard/statistics/index.tsx
+++ src/pages/dashboard/statistics/index.tsx
@@ -232,17 +232,17 @@ function Statistics() {
                                     <Chart
                                         type="area"
                                         series={[
-                                            {name: 'patients', data: patientPerPeriod.splice(0, 12)},
+                                            {name: 'patients', data: patientPerPeriod.slice(-12)},
                                             {
                                                 name: 'appointments',
-                                                data: appointmentPerPeriod.splice(0, 12)
+                                                data: appointmentPerPeriod.slice(-12)
                                             },
                                         ]}
                                         options={merge(ChartsOption(), {
                                             xaxis: {
                                                 position: "top",
                                                 categories: appointmentPerPeriodKeys.map(date =>
-                                                    startCase(moment(date, "DD-MM-YYYY HH:mm").format(VIEW_OPTIONS.find(view => view.value === viewChart)?.format).replace('.', ''))).splice(0, 12)
+                                                    startCase(moment(date, "DD-MM-YYYY HH:mm").format(VIEW_OPTIONS.find(view => view.value === viewChart)?.format).replace('.', ''))).slice(-12)
                                             },
                                             tooltip: {x: {show: false}, marker: {show: false}},
                                             colors: ['#1BC47D', '#FEC400'],
