c7cd3c78063914f7d1f56b6250363a67e5408f87
MED <mohamed.zghal@med.tn>
2024-01-15 17:50:17 +0100

    fix: add drugs actions

--- src/pages/dashboard/settings/drugs/index.tsx
+++ src/pages/dashboard/settings/drugs/index.tsx
@@ -1,5 +1,16 @@
 import {SubHeader} from "@features/subHeader";
-import {Box, Button, Drawer, Stack, Theme, Typography, useMediaQuery, useTheme} from "@mui/material";
+import {
+    Box,
+    Button, Dialog, DialogActions,
+    DialogContent,
+    DialogTitle,
+    Drawer,
+    Stack,
+    Theme,
+    Typography,
+    useMediaQuery,
+    useTheme
+} from "@mui/material";
 import React, {ReactElement, useState} from "react";
 import {DesktopContainer} from "@themes/desktopConainter";
 import {useTranslation} from "next-i18next";
@@ -7,33 +18,42 @@ import {GetStaticProps} from "next";
 import {serverSideTranslations} from "next-i18next/serverSideTranslations";
 import {configSelector, DashLayout} from "@features/base";
 import {LoadingScreen} from "@features/loadingScreen";
-import {useRequestQuery} from "@lib/axios";
+import {useRequestQuery, useRequestQueryMutation} from "@lib/axios";
 import {ReactQueryNoValidateConfig} from "@lib/axios/useRequestQuery";
 import {useRouter} from "next/router";
 import {Otable} from "@features/table";
 import {DrugsDrawer} from "@features/drawer";
 import {useAppSelector} from "@lib/redux/hooks";
 import {useMedicalProfessionalSuffix} from "@lib/hooks";
+import CloseIcon from "@mui/icons-material/Close";
+import {LoadingButton} from "@mui/lab";
+import Icon from "@themes/urlIcon";
+import {useSnackbar} from "notistack";
 
 function Drugs() {
     const theme: Theme = useTheme();
     const isMobile = useMediaQuery(theme.breakpoints.down("md"));
     const router = useRouter();
     const {urlMedicalProfessionalSuffix} = useMedicalProfessionalSuffix();
+    const {enqueueSnackbar} = useSnackbar();
 
     const {t, ready} = useTranslation(["settings", "common"], {keyPrefix: "drugs.config"});
     const {direction} = useAppSelector(configSelector);
 
     const [edit, setEdit] = useState(false);
     const [selectedDrug, setSelectedDrug] = useState<any>(null);
+    const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
+    const [loadingRequest, setLoadingRequest] = useState(false);
 
-    const {data: drugsResponse, mutate: mutateDrugs} = useRequestQuery({
+    const {data: drugsResponse, mutate: mutateDrugs} = useRequestQuery(urlMedicalProfessionalSuffix ? {
         method: "GET",
         url: `${urlMedicalProfessionalSuffix}/drugs/${router.locale}`
-    }, {
+    } : null, {
         ...ReactQueryNoValidateConfig,
-        variables: {query: !isMobile ? `?page=${router.query.page || 1}&limit=10&withPagination=true&sort=true` : "?sort=true"}
+        ...(urlMedicalProfessionalSuffix && {variables: {query: !isMobile ? `?page=${router.query.page || 1}&limit=10&withPagination=true&sort=true` : "?sort=true"}})
     });
+
+    const {trigger: deleteDrugsTrigger} = useRequestQueryMutation("/settings/drug/delete");
 
     const headCells = [
         {
@@ -79,8 +99,30 @@ function Drugs() {
     ];
     const drugs = ((drugsResponse as HttpResponse)?.data ?? []) as DrugModel[];
 
+    const removeDrug = (uuid: string) => {
+        setLoadingRequest(true)
+        urlMedicalProfessionalSuffix && deleteDrugsTrigger({
+            method: "DELETE",
+            url: `${urlMedicalProfessionalSuffix}/drugs/${uuid}/${router.locale}`
+        }, {
+            onSuccess: () => {
+                enqueueSnackbar(t("alert.delete"), {variant: "success"});
+                setTimeout(() => setOpenDeleteDialog(false));
+                mutateDrugs();
+            },
+            onSettled: () => setLoadingRequest(false)
+        });
+    }
+
     const handleTableActions = (data: any) => {
+        setSelectedDrug(data?.row);
         switch (data.action) {
+            case "DELETE_DRUGS":
+                setOpenDeleteDialog(true);
+                break;
+            case "EDIT_DRUGS":
+                setEdit(true);
+                break;
 
         }
     }
@@ -101,7 +143,10 @@ function Drugs() {
                     alignItems="center">
                     <Typography color="text.primary">{t("path")}</Typography>
                     <Button
-                        onClick={() => setEdit(true)}
+                        onClick={() => {
+                            setSelectedDrug(null);
+                            setTimeout(() => setEdit(true));
+                        }}
                         variant="contained"
                         color="success"
                         sx={{ml: "auto"}}>
@@ -130,6 +175,49 @@ function Drugs() {
                     data={selectedDrug}
                 />
             </Drawer>
+            <Dialog
+                PaperProps={{
+                    sx: {
+                        width: "100%",
+                        m: 1
+                    }
+                }} maxWidth="sm" open={openDeleteDialog}>
+                <DialogTitle sx={{
+                    bgcolor: (theme: Theme) => theme.palette.error.main,
+                    px: 1,
+                    py: 2,
+
+                }}>
+                    {t("dialog.delete.title")}
+                </DialogTitle>
+                <DialogContent style={{paddingTop: 20}}>
+                    <Typography>
+                        {t("dialog.delete.desc")}
+                    </Typography>
+                </DialogContent>
+                <DialogActions sx={{borderTop: 1, borderColor: "divider", px: 1, py: 2}}>
+                    <Stack direction="row" spacing={1}>
+                        <Button
+                            variant="text-black"
+                            onClick={() => {
+                                setOpenDeleteDialog(false);
+                                setTimeout(() => setSelectedDrug(null));
+                            }}
+                            startIcon={<CloseIcon/>}>
+                            {t("dialog.cancel")}
+                        </Button>
+                        <LoadingButton
+                            loading={loadingRequest}
+                            loadingPosition={"start"}
+                            variant="contained"
+                            color="error"
+                            onClick={() => removeDrug(selectedDrug?.uuid as string)}
+                            startIcon={<Icon path="setting/icdelete" color="white"/>}>
+                            {t("dialog.delete.delete")}
+                        </LoadingButton>
+                    </Stack>
+                </DialogActions>
+            </Dialog>
         </>
     )
 }
