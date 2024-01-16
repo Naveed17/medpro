87d91f84e9672ed6e0a5bf4e2550f285a6f002c7
Wassim ayedi <wassim.ayadi@med.tn>
2024-01-16 10:12:25 +0100

    fix ably connection

--- src/lib/ably/ablyClient.tsx
+++ src/lib/ably/ablyClient.tsx
@@ -1,9 +1,11 @@
 import {AblyProvider} from 'ably/react';
 import * as Ably from 'ably';
+import {useMemo, useState} from "react";
 
 function AblyClient({children, ...pageProps}: any) {
-    const client = new Ably.Realtime.Promise({authUrl: '/api/token', authMethod: 'POST'});
+   const client = useMemo(()=> new Ably.Realtime.Promise({authUrl: '/api/token', authMethod: 'POST'}),[]) ;
 
+    console.log("connectionID",client.connection.id)
     return (<AblyProvider {...{client}}>
         {children}
     </AblyProvider>)
