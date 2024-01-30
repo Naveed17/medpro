694b497079becdd12d933278cba46e613b912a7e
Wassim ayedi <wassim.ayadi@med.tn>
2024-01-30 07:52:50 +0100

    complete translation

--- src/features/stepper/components/customStepper.tsx
+++ src/features/stepper/components/customStepper.tsx
@@ -1,10 +1,11 @@
-import React, { useState, useCallback, useEffect } from "react";
-import { RootStyled,setStepperIndex} from "@features/stepper";
+import React, {useCallback, useEffect, useState} from "react";
+import {RootStyled, setStepperIndex} from "@features/stepper";
 import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
-import {useAppDispatch } from "@lib/redux/hooks";
-function CustomStepper({ ...props }) {
+import {useAppDispatch} from "@lib/redux/hooks";
+
+function CustomStepper({...props}) {
     const dispatch = useAppDispatch();
     const {
         tabIndex,
@@ -13,25 +14,25 @@ function CustomStepper({ ...props }) {
         t,
     } = props;
     const [index, setIndex] = useState<number>(tabIndex);
+
     const tabChange = useCallback(
         (currentIndex: number) => {
             setIndex(currentIndex);
             dispatch(setStepperIndex(currentIndex));
         },
-        [index]
+        [index] // eslint-disable-line react-hooks/exhaustive-deps
     );
+
     useEffect(() => {
         setIndex(tabIndex)
     }, [tabIndex])
-
-
 
     return (
         <>
             <RootStyled
                 sx={{
-                    minWidth: { md: minWidth ? minWidth : "100%", xs: "100%" },
-                    maxWidth: { md: minWidth ? minWidth : "100%", xs: "100%" },
+                    minWidth: {md: minWidth ? minWidth : "100%", xs: "100%"},
+                    maxWidth: {md: minWidth ? minWidth : "100%", xs: "100%"},
                 }}
             >
                 <Stepper activeStep={index} alternativeLabel>
@@ -45,7 +46,7 @@ function CustomStepper({ ...props }) {
                             <Step
                                 key={v.title}
                                 className={
-                                    i === index && stepperData.length - 1 === index ? "last-step":
+                                    i === index && stepperData.length - 1 === index ? "last-step" :
                                         stepperData.length - 1 < index
                                             ? "Mui-completed"
                                             : i === index
