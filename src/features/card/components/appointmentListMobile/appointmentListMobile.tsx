fbebc087eb0ca5bc40ede4ebcb5dd4eb22abb70d
MED <mohamed.zghal@med.tn>
2024-02-01 12:34:51 +0100

    fix: mobile view event list name order

--- src/features/card/components/appointmentListMobile/appointmentListMobile.tsx
+++ src/features/card/components/appointmentListMobile/appointmentListMobile.tsx
@@ -129,7 +129,7 @@ function AppointmentListMobile({...props}) {
                                         {...(event.status?.key === "WAITING_ROOM" && {pl: 0.5})}
                                         variant="body2"
                                         fontWeight={600}>
-                                        {event.patient.lastName} {event.patient.firstName}
+                                        {event.patient.firstName}{event.patient.lastName}
                                     </Typography>}
                                 </Stack>
                             </Stack>
