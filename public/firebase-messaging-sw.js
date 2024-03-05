2bbc0d5a7d6212946160fae8a0f92d18b39e3c43
MED <mohamed.zghal@med.tn>
2024-03-05 17:49:50 +0100

    fix: fix dependency vulnerability

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/10.7.2/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/10.7.2/firebase-messaging-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.8.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.8.1/firebase-messaging-compat.js");
 
 firebase.initializeApp({
     apiKey: "AIzaSyCvG726dE6kIYfGaqc90pEfirK5lSdLUpM",
