4b9a2c77212c497deb4d65c577ef391c8d89922c
MED <mohamed.zghal@med.tn>
2024-03-28 11:29:52 +0100

    fix: utc moment issues

--- src/lib/hooks/getDiffDuration.ts
+++ src/lib/hooks/getDiffDuration.ts
@@ -6,5 +6,6 @@ const humanizeDuration = require("humanize-duration");
 export const getDiffDuration = (date: string, largest = 2, round = true) => {
     const shortEnglishHumanizer = humanizeDuration.humanizer(humanizerConfig);
     const duration: any = moment.duration(moment.utc().diff(moment.utc(date, "DD-MM-YYYY HH:mm")));
+
     return shortEnglishHumanizer(duration, {largest, round});
 }
