1676ee4b0288857b0b91873808eafa5f0d35fce0
MED <mohamed.zghal@med.tn>
2024-04-09 06:08:22 +0100

    fix: optimize image fix: update firebase package

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/10.8.1/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/10.8.1/firebase-messaging-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.10.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.10.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
     apiKey: "AIzaSyCvG726dE6kIYfGaqc90pEfirK5lSdLUpM",
