0738e2a247d91426681cd2835d75ab088fafba05
MED <mohamed.zghal@med.tn>
2024-04-15 11:05:10 +0100

    fix: api/see undefined data

--- src/features/base/components/mainLayout/mainLayout.tsx
+++ src/features/base/components/mainLayout/mainLayout.tsx
@@ -341,6 +341,7 @@ function MainLayout({...props}) {
         // So that it attempts to connect again
         eventSource.onerror = (error) => {
             eventSource.close();
+            console.log("error", error);
             if ((error as any)?.data) {
                 const errorData = (error as any).data && JSON.parse((error as any)?.data);
                 if (![404, 500, 502, 503, 504].includes(errorData?.status)) {
