17a882a2b7b80e48cb50d64ddb7b93381f3d129b
Wassim ayedi <wassim.ayadi@med.tn>
2024-04-16 17:21:42 +0100

    fix: english edit consultation reason error name

--- src/features/dialog/components/editMotifDialog/components/editMotifDialog.tsx
+++ src/features/dialog/components/editMotifDialog/components/editMotifDialog.tsx
@@ -130,7 +130,7 @@ function EditMotifDialog({ ...props }) {
             if (props.data) {
                 medicalEntityHasUser && triggerMotifUpdate({
                     method: "PUT",
-                    url: `${urlMedicalEntitySuffix}/mehu/${medicalEntityHasUser}/consultation-reasons/${props.data.uuid}/${router.locale}`,
+                    url: `${urlMedicalEntitySuffix}/mehu/${medicalEntityHasUser}/consultation-reasons/${props.data.uuid}/fr`,
                     data: form
                 }, {
                     onSuccess: () => {
