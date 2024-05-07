38fd29e4d6bf187c70a401fa8d6910bf5c471d06
MED <mohamed.zghal@med.tn>
2024-05-07 16:03:32 +0100

    fix: code optimization

--- src/lib/hooks/unflattenObject.ts
+++ src/lib/hooks/unflattenObject.ts
@@ -1,10 +1,11 @@
 export function unflattenObject(ob: any) {
-    return Object.keys(ob).reduce((res, k) =>
-        k.split('.').reduce(
-            (acc, e, i, keys) =>
+    return Object.keys(ob).reduce((res, k) => {
+        k.split('.').reduce((acc, e, i, keys) =>
                 acc[e] || (acc[e] = isNaN(Number(keys[i + 1]))
                     ? keys.length - 1 === i
                         ? ob[k]
                         : {}
-                    : []), res), {} as any)
+                    : []), res);
+        return res;
+    }, {} as any)
 }
