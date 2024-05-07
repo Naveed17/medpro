05fc2772f6238b755c1c3d108c737dc227466762
MED <mohamed.zghal@med.tn>
2024-05-07 17:48:23 +0100

    fix: not insured patient flag

--- src/lib/hooks/generateFilter.ts
+++ src/lib/hooks/generateFilter.ts
@@ -1,8 +1,8 @@
 export const generateFilter = ({...props}) => {
-    const{filterCB} = props;
+    const {filterCB} = props;
     let query = '';
     Object.keys(filterCB).forEach(key => {
-        if (filterCB[key] && filterCB[key].length > 0) {
+        if (filterCB[key] && (filterCB[key].length > 0 || Boolean(filterCB[key]))) {
             query.length === 0 ? query += `?${key}=${filterCB[key]}` : query += `&${key}=${filterCB[key]}`;
         }
     });
