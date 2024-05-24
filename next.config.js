35756365a6ceee3321176caa8a2ea5bc8ebde8e1
MED <mohamed.zghal@med.tn>
2024-05-24 03:40:03 +0100

    fix: warning issues

--- next.config.js
+++ next.config.js
@@ -57,6 +57,7 @@ const nextConfig = {
             }]
     },
     webpack: (config) => {
+        config.resolve.alias.canvas = false;
         config.module.rules.push({
             test: /\.svg$/,
             use: ["@svgr/webpack", "url-loader"]
